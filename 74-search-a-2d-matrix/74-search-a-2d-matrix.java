1920007e1f44c56f1e26c89deeb0d85f48039842
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-04-01 08:14:31 +0900

    Time: 1 ms (33.12%), Space: 42.6 MB (56.96%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+          for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+
+                    if(target>matrix[i][matrix[i].length-1]){
+                        break;
+                    }else if(target==matrix[i][j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+    }
+}
