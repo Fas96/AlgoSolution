aad26a19c4f3fca6d225f5858fc149414b627bed
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-04-11 21:45:12 +0900

    Time: 23 ms (6.16%), Space: 45.8 MB (6.16%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        
-    ArrayList<Integer> a = new ArrayList<>(nums.length);
+     
+        ArrayList<Integer> a = new ArrayList<>(nums.length);
+
         for (int n : nums) {a.add(n);}
         int[]  res= new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            res[i]=sumList(new ArrayList<>(a.subList(0, i+1)));
+            res[i]= (int) a.subList(0, i+1).stream().mapToLong(Integer::longValue).sum();;
         }
         return res;
-    }
-
-    private   int sumList(List<Integer> collect) {
-        int t=0;
-        for (Integer n : collect) {t+=n;}
-        return t;
     }
 }
