c3404552cd387f5ae3e397d812f9168f95d421f7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-04-14 23:53:18 +0900

    Time: 0 ms (100.00%), Space: 42.7 MB (85.98%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -15,20 +15,15 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-          // Case where root is equal to null meaning we have reached the end of the tree traversal
-        if(root == null){
-            return null;
-        }
-        
-        if(root.val == val){
-			// Case where root value is equal to value searched, TreeNode found
-            return root;
-        }else if(root.val > val){
-            // Case where the root value is greater than value searched, therefore we have to search on left side
-            return searchBST(root.left, val);
-        }else{
-            // Case where the root value is smaller than value searched, therefore we have to search on right side
-            return searchBST(root.right, val);
-        }
+         if (root == null) {
+                return null;
+            } 
+            if(root.val==val){
+                return root;
+            }else if (root.val>val){
+                return searchBST(root.left,val);
+            }else {
+                return searchBST(root.right,val);
+            }
     }
 }
