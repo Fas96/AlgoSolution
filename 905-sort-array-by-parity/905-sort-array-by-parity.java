f81d0bdc2d998405cfe4a19ced98eb61050ef201
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-05-02 23:57:04 +0900

    Time: 27 ms (5.01%), Space: 48.7 MB (15.61%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,19 +1,10 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-           int[] events = Arrays.stream(nums).filter(e -> e % 2 == 0).toArray();
-        int[] ords = Arrays.stream(nums).filter(e -> e % 2 == 1).toArray();
-        int[] resa=new int[nums.length];
-        List<Integer> res= new ArrayList<>();
-        for (int e : events) {
-            res.add(e);
-        }
-        for (int e : ords) {
-            res.add(e);
-        }
-        for (int i = 0; i < res.size(); i++) {
-            resa[i]=res.get(i);
-        }
-
-        return resa; 
+          return Arrays
+		.stream(nums)
+		.boxed()
+		.sorted((a, b) -> Integer.compare(a % 2, b % 2))
+		.mapToInt(Integer::intValue)
+		.toArray();
     }
 }
