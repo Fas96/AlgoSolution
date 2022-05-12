c3028a75be4e941c00e14f1d10b662caa4ef1e18
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-05-12 17:34:40 +0900

    Time: 114 ms (34.50%), Space: 19 MB (5.18%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        res=[]
+        for ls in set(list(permutations(nums, len(nums)))):
+            res.append(list(ls))
+        print(res)
+        
+        return res
+        
