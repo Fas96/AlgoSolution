3333faaff119c26ad193e09be243618b0a959741
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-05-14 09:05:19 +0900

    Time: 82 ms (26.79%), Space: 15.2 MB (93.06%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -1,18 +1,20 @@
 class Solution:
-    from collections import deque
     def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return 
-        queue = deque()
-        queue.append([root, 0])
-        prev, lvl = None, -1
-        while queue:
-            current, level = queue.popleft()
-            if current.left:
-                queue.append([current.left, level + 1])
-            if current.right:
-                queue.append([current.right, level + 1])
-            if prev and level == lvl:
-                prev.next = current
-            prev, lvl = current, level
-        return root  
+        # Empty tree
+        if root == None:
+            return root
+        # Level order traversal keeping the track of next node in prev
+        queue = [root]
+        while len(queue) > 0:
+            size = len(queue)
+            prev = None
+            while size > 0:
+                node = queue.pop(0)
+                size = size - 1
+                node.next = prev
+                prev = node
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+        return root
