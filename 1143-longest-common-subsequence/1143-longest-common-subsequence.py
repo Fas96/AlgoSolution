d68b3ed6a74ed14c102cae276f99b8d6e0c4b8f2
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-06-07 10:14:00 +0900

    Time: 368 ms (58.73%), Space: 21.8 MB (64.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -8,14 +8,7 @@ class Solution(object):
         """
         n = len(text1)
         m = len(text2)
-        # dp = [[-1] * (len(text2) + 1)] * (len(text1) + 1)
-#         mx = 0
-        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-    
-        for j in range(0,m+1):
-            dp[0][j] = 0
-        for i in range(0,n+1):
-            dp[i][0] = 0
+        dp = [[0] * (m+1) for _ in range(n+1)]
         for i in range(1, n + 1):
             for j in range(1, m + 1):
                 if text1[i - 1] == text2[j - 1]:
