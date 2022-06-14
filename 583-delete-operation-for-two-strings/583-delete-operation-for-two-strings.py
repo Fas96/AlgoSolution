b28da43f4fc5434bddf9f88503a64ee931842c38
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-06-14 22:47:23 +0900

    Time: 372 ms (30.72%), Space: 15.5 MB (83.74%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,17 +1,15 @@
-class Solution:       
-    def minDistance(self, word1: str, word2: str) -> int:
-        if len(word1)>len(word2):
-            word2,word1=word1,word2        
-        
-        m,n=len(word1),len(word2)
-        prev=[0] * (m+1)
-        
-        for i in range(n-1, -1, -1):
-            curr=[0] * (m+1)
-            for j in range(m-1, -1, -1):
-                if word1[j] == word2[i]:
-                    curr[j]=1 + prev[j+1]
+class Solution(object):
+    def minDistance(self, word1, word2):
+
+        mx = len(word2)
+        mi = len(word1)
+
+        dp = [[0] * (mx + 1) for i in range(mi + 1)]
+        for i in range(mi):
+            for j in range(mx):
+                if word1[i] == word2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
                 else:
-                    curr[j]=max(curr[j+1], prev[j])
-            prev=curr
-        return m + n - 2*prev[0]
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return (mi + mx) - (2 * dp[-1][-1])
