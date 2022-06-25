191d52e2964e09c81a74d3f261e211b86f81f03c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-06-25 21:25:43 +0900

    Time: 323 ms (5.63%), Space: 14.6 MB (54.93%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def checkPossibility(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        one, two = nums[:], nums[:]
-        print(one,two)
+        l = nums[:]
+        k = nums[:]
+
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
-                one[i] = nums[i + 1]
-                two[i + 1] = nums[i]
+                l[i] = nums[i + 1]
+                k[i + 1] = nums[i]
                 break
-         
-        return one == sorted(one) or two == sorted(two)
+        return l == sorted(l) or k == sorted(k)
