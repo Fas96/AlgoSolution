32787eeb13db0deeae51b1945fe663b8dfa94929
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-06-25 21:12:58 +0900

    Time: 594 ms (5.63%), Space: 14.7 MB (54.93%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        one, two = nums[:], nums[:]
+        print(one,two)
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                one[i] = nums[i + 1]
+                two[i + 1] = nums[i]
+                break
+         
+        return one == sorted(one) or two == sorted(two)
