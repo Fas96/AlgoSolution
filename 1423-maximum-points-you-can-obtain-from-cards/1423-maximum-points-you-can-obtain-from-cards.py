b5c594576549be90a65a90d73794f09ba0e67563
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-06-26 23:10:49 +0900

    Time: 361 ms (83.40%), Space: 23.6 MB (72.33%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxScore(self, cardPoints, k):
+        N = len(cardPoints)
+        i = 0
+        j = N - k
+        total = sum(cardPoints[j:])
+        best = total
+        for _ in range(k):  
+            total += cardPoints[i] - cardPoints[j]
+            best = max(best, total)
+            i += 1
+            j += 1
+        return best
+
+        
