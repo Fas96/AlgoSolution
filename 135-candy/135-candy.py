4dd65c957ffed10558ee540b4855fff562daadfe
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-07-05 00:02:35 +0900

    Time: 273 ms (38.56%), Space: 16.9 MB (37.74%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -9,31 +9,28 @@ class Solution:
         for i in range(n-1):
             if ratings[i]<ratings[i+1]:
                 dp[i+1]=max(dp[i]+1,dp[i+1])
+        print(dp)
         for i in range(n-2,-1,-1):
             if ratings[i+1]<ratings[i]:
                 dp[i]=max(dp[i+1]+1,dp[i])
                 
         
         
-#         L,R=0,1
+        # L,R=0,1
 #         while(R<n):
-#             if ratings[L] > ratings[R]:
-#                 if dp[L]<=min(dp[R],dp[L]):
-#                     dp[L]+=min(dp[R],dp[L])
+#             if ratings[L] < ratings[R]:
+#                 dp[L+1]=max(dp[L]+1,dp[R])
 #                 L+=1
 #                 R+=1
-#             elif ratings[L] < ratings[R]:
-#                 if dp[R]<=min(dp[R],dp[L]):
-#                     dp[R]+=min(dp[R],dp[L])
-                
+#             elif ratings[L] > ratings[R]:
+#                 dp[R]=max(dp[R]+1,dp[L])
 #                 L+=1
 #                 R+=1
 #             else:
 #                 L+=1
 #                 R+=1
                 
-#         print(dp)
-
+ 
         return sum(dp)
     
      
