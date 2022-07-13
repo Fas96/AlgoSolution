8bbbc892caf194b59d8878238afd65564a596b25
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-07-14 08:12:03 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (91.94%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,32 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        levelHelper(res, root, 0);
-        return res;
+    List<List<Integer>> ans = new ArrayList<>();
+      callStackLevels(ans,root,0);
+    return ans;
     }
-    
-    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
-        if (root == null) return;
-        if (height >= res.size()) {
-            res.add(new LinkedList<Integer>());
-        }
-        res.get(height).add(root.val);
-        levelHelper(res, root.left, height+1);
-        levelHelper(res, root.right, height+1);
+    public void callStackLevels(List<List<Integer>>  lelVals,TreeNode root,int level) {
+      if(root==null) return;
+      if(level>=lelVals.size()){
+        lelVals.add(new LinkedList<Integer>());
+      }
+      lelVals.get(level).add(root.val );
+      callStackLevels(lelVals,root.left,level+1);
+      callStackLevels(lelVals,root.right,level+1);
     }
-}
+  }
