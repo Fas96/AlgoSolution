373dc9b48403c09ce3d873e7ad6369df3a7c5718
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-15 16:59:09 +0900

    Time: 6 ms (85.69%), Space: 43.1 MB (85.28%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0;
+   int res = 0;
     Map<Character, Integer> map = new HashMap<Character, Integer>() {{
       put('I', 1);
       put('V', 5);
@@ -10,19 +10,21 @@ class Solution {
       put('D', 500);
       put('M', 1000);
     }};
-    for (int i = 0; i < s.length(); i++){
-      int curVal=0;
-      if(i+1<s.length() && s.charAt(i)=='I' && (s.charAt(i+1)=='V'||s.charAt(i+1)=='X')){
-          curVal=-1;
-        }else if ( i+1<s.length() && s.charAt(i)=='X' && (s.charAt(i+1)=='L'||s.charAt(i+1)=='C')){
-          curVal=-10;
-        }
-        else if(i+1<s.length() && s.charAt(i)=='C' && (s.charAt(i+1)=='D'||s.charAt(i+1)=='M')){
-          curVal=-100;
-        }else {
-          curVal=map.get(s.charAt(i));
-        }
-      res+=curVal;
+    for (int i = 0; i < s.length(); i++) {
+      int curVal = 0;
+      if (i + 1 < s.length() && s.charAt(i) == 'I' && (s.charAt(i + 1) == 'V'
+          || s.charAt(i + 1) == 'X')) {
+        curVal = -1;
+      } else if (i + 1 < s.length() && s.charAt(i) == 'X' && (s.charAt(i + 1) == 'L'
+          || s.charAt(i + 1) == 'C')) {
+        curVal = -10;
+      } else if (i + 1 < s.length() && s.charAt(i) == 'C' && (s.charAt(i + 1) == 'D'
+          || s.charAt(i + 1) == 'M')) {
+        curVal = -100;
+      } else {
+        curVal = map.get(s.charAt(i));
+      }
+      res += curVal;
     }
     return res;
     }
