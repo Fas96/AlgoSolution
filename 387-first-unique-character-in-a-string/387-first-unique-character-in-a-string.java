2afeeb9354ea9eaca4095bc832157273969737eb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-16 09:35:10 +0900

    Time: 36 ms (55.31%), Space: 50 MB (51.45%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,19 +1,12 @@
 class Solution {
 public int firstUniqChar(String s) {
  
-    int res=-1;
-    List<Character> keeper=new ArrayList<>();
-    for (int i = 0; i < s.length(); i++) {
-      int finalI = i; 
-      if (keeper.contains(s.charAt(finalI)))continue;
-      long count = s.chars().filter(ch -> ch == s.charAt(finalI)).count();
-      if(count==1 && !keeper.contains(s.charAt(finalI))){
-        return finalI;
-      }else{
-        keeper.add(s.charAt(finalI));
-      }
-    }
-    return res;
+      char[] a = s.toCharArray();
+		
+		for(int i=0; i<a.length;i++){
+			if(s.indexOf(a[i])==s.lastIndexOf(a[i])){return i;}
+		}
+		return -1;
 
   }
 }
