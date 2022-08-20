38fa5f000ab0a5534270f0462b9748fb8ac65b98
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-20 16:16:03 +0900

    Time: 4 ms (86.38%), Space: 49.9 MB (8.12%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,3 +1,14 @@
+/*
+
+ long[] dp = new long[s.length + 1];
+        dp[0] = startFuel;
+        for (int i = 0; i < s.length; ++i)
+            for (int t = i; t >= 0 && dp[t] >= s[i][0]; --t)
+                dp[t + 1] = Math.max(dp[t + 1], dp[t] + s[i][1]);
+        for (int t = 0; t <= s.length; ++t)
+            if (dp[t] >= target) return t;
+        return -1;
+*/
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
        int n = stations.length;
