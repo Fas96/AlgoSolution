5e2939b05ad62077e1303c93df81cdfade8378a8
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-22 00:29:41 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (66.75%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,24 +1,19 @@
 class Solution {
     public int numTrees(int n) {
-        long k =1;
+     
+        int res = Math.toIntExact(factorialXTwo(n));
+        return res;
+    }
+ long factorialXTwo(int n) {
+      long k =1;
         for (int i = 2; i <= n; i++) 
         {
             k = 2*(2*i-1)*k/(i+1);
         }
-        int res = Math.toIntExact(k);
-        return res;
-    }
- int factorial(int n) {
-      int fact = 1;
-      int i = 1;
-      while(i <= n) {
-         fact *= i;
-         i++;
-      }
-      return fact;
+     return k;
    }
     
-// nCr  =  	n!
+//   nCr  =  	n!
 //   	 r!(n - r)!
 
        // comb = factorial(n) / (factorial(r) * factorial(n-r));
