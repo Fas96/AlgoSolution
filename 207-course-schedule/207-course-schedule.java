8ebaf3c10e3b091de4d78411d053b907c0d7931e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-24 22:37:50 +0900

    Time: 34 ms (11.63%), Space: 46.7 MB (82.31%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int[] table= new int[numCourses];
+    for (int[] course : prerequisites) {
+      int curCourse= course[0];
+      table[curCourse]++;
+    }
+    Set<Integer> noIncomingEdges= new HashSet<>();
+    //find elements that has no incoming edges
+    for (int i = 0; i < numCourses; i++) {
+      if(table[i]==0)noIncomingEdges.add(i);
+    }
+    //if set is empty that means we have cyclic graph
+    if(noIncomingEdges.isEmpty())return false;
+    
+    while (!noIncomingEdges.isEmpty()){
+      Iterator<Integer> it=noIncomingEdges.iterator();
+      Integer element=  it.next();
+      //remove the edges the current element has
+      for (int course = 0; course < prerequisites.length; course++) {
+        //check if prerequisite is equals to current element
+        if(prerequisites[course][1]==element){
+          int curCourse=prerequisites[course][0];
+          //remove the incoming edges of this current course
+          table[curCourse]--;
+          if(table[curCourse]==0){
+            noIncomingEdges.add(curCourse);
+          }
+        }
+      }
+      noIncomingEdges.remove(element);
+    }
+    
+    //check if there is cycle
+    for (int course :table) {
+      if(course>0)return false;
+    }
+    
+    return true;
+  }
+}
