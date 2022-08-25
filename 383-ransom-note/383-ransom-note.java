76e8ef1c01136d8e479a938aeffa9e7f4a079cbc
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-25 09:19:57 +0900

    Time: 27 ms (35.02%), Space: 51.7 MB (35.29%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
     Map<Character,Integer> rMap= new HashMap<>();
     Map<Character,Integer> mMap= new HashMap<>();
+        
     for (int i = 0; i < ransomNote.length(); i++) {
       rMap.merge(ransomNote.charAt(i),1,Integer::sum);
     }
@@ -9,7 +10,7 @@ class Solution {
       mMap.merge(magazine.charAt(i),1,Integer::sum);
     }
 
-  return   rMap.entrySet().stream().allMatch(e->mMap.getOrDefault(e.getKey(),0)>=e.getValue());
+    return   rMap.entrySet().stream().allMatch(e->mMap.getOrDefault(e.getKey(),0)>=e.getValue());
 
     }
 }
