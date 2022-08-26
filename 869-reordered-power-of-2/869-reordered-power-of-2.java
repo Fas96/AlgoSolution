11e4f2a1ac906075fc9d1ed1099cf2a828366966
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-26 10:10:03 +0900

    Time: 3 ms (44.67%), Space: 42.5 MB (6.00%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,18 +1,17 @@
 class Solution {
   public boolean reorderedPowerOf2(int n) { 
      int x = 1;
-    char[] giveCharArr=(""+n).toCharArray();
-    Arrays.sort(giveCharArr); 
+     char[] giveCharArr=(""+n).toCharArray();
+     Arrays.sort(giveCharArr); 
       
-      for (int i = 1; i < 1000000000; ){
+  for (int i = 1; i < 1000000000; ){
       char[] c = ("" + i).toCharArray();
       Arrays.sort(c);
-
+      
       if((new String(c)).equals(new String(giveCharArr))) return true;
       i <<= 1;
     }
       
-    
     return false;
   }
   
