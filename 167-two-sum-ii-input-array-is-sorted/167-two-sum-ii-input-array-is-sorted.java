b2bebef6f8245d5ed9c7d23929ca853d903f2782
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-26 18:05:58 +0900

    Time: 2 ms (80.12%), Space: 50.2 MB (45.70%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-         int startInc=0,endDec=0;
-    int N=numbers.length;
-    for (int i = 0; i <N ; i++) {
-      if(numbers[startInc]+numbers[N-1-endDec]==target){
-       
-        return new int[]{startInc+1,N-1-endDec+1};
-      } else if (numbers[startInc]+numbers[N-1-endDec]<target) {
+    int startInc=0,endDec=numbers.length-1;
+    while (startInc<endDec) {
+      if(numbers[startInc]+numbers[endDec]==target){
+        return new int[]{startInc+1,endDec+1};
+      } else if (numbers[startInc]+numbers[endDec]<target) {
         startInc++;
-      } else if (numbers[startInc]+numbers[N-1-endDec]>target) {
-        endDec++;
+      } else if (numbers[startInc]+numbers[endDec]>target) {
+        endDec--;
       }
     }
     return new int[]{};  
