41d3fbea8a9feaf835de74b54fa3a6305f8db9c9
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-30 09:31:06 +0900

    Time: 1 ms (7.90%), Space: 43 MB (29.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,21 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        
-        for(int first=0,last=rows-1; first<last; first++, last--){
-            int[] tmp = matrix[first];
-            matrix[first] = matrix[last];
-            matrix[last] = tmp;
-        }
-        
-        for(int i=0;i<rows;i++){
-            for(int j=i+1;j<cols;j++){
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
-            }
-        }
+        	int n = matrix.length;
+	for (int i = 0; i < n / 2; i++) {
+		for (int j = 0; j < Math.ceil(((double) n) / 2.); j++) {
+			int temp = matrix[i][j];
+			matrix[i][j] = matrix[n-1-j][i];
+			matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+			matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+			matrix[j][n-1-i] = temp;
+		}
+	}
     }
 }
