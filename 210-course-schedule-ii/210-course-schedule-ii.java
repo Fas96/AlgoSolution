2c91fcb291714982e758e6e5e18d10bc7f7b9ca2
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-08-30 15:54:29 +0900

    Time: 29 ms (12.14%), Space: 48.6 MB (74.85%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,22 +1,27 @@
 class Solution {
- public int [] findOrder(int numCourses, int[][] prerequisites) {
+    
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
     int[] table= new int[numCourses];
     for (int[] course : prerequisites) {
       int curCourse= course[0];
       table[curCourse]++;
     }
+  
     Set<Integer> noIncomingEdges= new HashSet<>();
-    //find elements that has no incoming edges
+    int []res= new int[numCourses];
+    int resCnt=0;
     for (int i = 0; i < numCourses; i++) {
       if(table[i]==0)noIncomingEdges.add(i);
     }
+   
     //if set is empty that means we have cyclic graph
-    if(noIncomingEdges.isEmpty()) return new int[]{};
-    int res[]= new int[numCourses];
-     int index=0;
+    if(noIncomingEdges.isEmpty())return new int[]{};
+   
+            
     while (!noIncomingEdges.isEmpty()){
       Iterator<Integer> it=noIncomingEdges.iterator();
       Integer element=  it.next();
+       
       //remove the edges the current element has
       for (int course = 0; course < prerequisites.length; course++) {
         //check if prerequisite is equals to current element
@@ -29,15 +34,17 @@ class Solution {
           }
         }
       }
-        res[index++]=element;
       noIncomingEdges.remove(element);
+      res[resCnt++]=element;
     }
-    
+
     //check if there is cycle
     for (int course :table) {
-      if(course>0)return  new int[]{};
+      if(course>0)return new int[]{};
     }
-    
-    return res;
+        
+    return  res;
+
   }
+    
 }
