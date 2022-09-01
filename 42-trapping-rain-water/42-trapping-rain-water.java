3d6dae7508edfb4fba9f120392bcb83f89876e01
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-01 20:32:02 +0900

    Time: 2 ms (59.24%), Space: 48.5 MB (67.97%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+    int N=height.length;
+    if(N<3)return 0;
+    int[]left=new int[N];
+    for (int i = 0; i < N; i++) {
+      left[i]=i==0?height[i]:Math.max(left[i-1],height[i]);
+    }
+    int[]right=new int[N];
+    for (int i = N-1; i >=0 ; i--) {
+      right[i]=i==N-1?height[i]:Math.max(right[i+1],height[i]);
+    }
+    int sum=0;
+    for (int i = 0; i < N; i++) {
+      sum+=Math.min(left[i],right[i] )-height[i];
+    }
+    return sum;
+    }
+}
+
+// 1*(2-1) 
+// 2*(4)-2
+// 1
+    
