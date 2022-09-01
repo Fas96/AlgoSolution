b2f11d1fb29326bd7cc20eef1a601aac519577ae
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-02 07:49:45 +0900

    Time: 0 ms (100.00%), Space: 43.2 MB (5.41%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+   int n=nums.length;
+    int curIdx=0,R=n-1,L=0;
+
+    while (curIdx<=R){
+      if(nums[curIdx]==2){
+        swap( nums,curIdx,R);
+        R--;
+      } else if (nums[curIdx]==1) {
+        curIdx++;
+      }else {
+        swap( nums,curIdx,L);
+        L++;
+         curIdx++;
+      }
     }
+  }
+  private  void swap(int[] arr, int i, int j) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
 }
