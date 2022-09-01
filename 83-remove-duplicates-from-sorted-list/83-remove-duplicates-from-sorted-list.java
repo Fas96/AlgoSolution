bd6c3b7e3277cfedefc6a42377b579223fdbcf71
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-02 08:44:15 +0900

    Time: 2 ms (6.76%), Space: 44.9 MB (6.39%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -10,18 +10,25 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null){
-            return null;
-        }
-        ListNode cur=head;
-        while(cur!=null){
-           if(cur.next==null)break;
-            if(cur.val==cur.next.val){
-                    cur.next=cur.next.next;
-            }else{
-                cur=cur.next;
-            }
-        }
-        return head;
+    if (head == null || head.next == null)return head;
+    
+
+    ListNode dummyNode = new ListNode();
+    dummyNode.next = head;
+
+    ListNode prev = dummyNode, cur = head;
+
+    while (cur != null) {
+      if (cur.next != null && cur.val == cur.next.val) {
+        // while (cur.next != null && cur.val == cur.next.val)cur = cur.next;
+        
+        prev.next=cur.next;
+      } else {
+        prev = cur;
+      }
+      cur=cur.next;
+    }
+
+    return dummyNode.next;
     }
 }
