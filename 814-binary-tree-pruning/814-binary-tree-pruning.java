d3448e29f2acf756676c4d76ae2531d49e7c7a8b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-06 12:39:59 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (58.05%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -15,20 +15,14 @@
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        if(root==null) return null;
+        return dfs(root);
         
+    }
+    private TreeNode dfs(TreeNode root){
+         if(root==null) return null;
         root.left=dfs(root.left);
         root.right=dfs(root.right);
         return   (root.val == 1 || root.left != null || root.right != null) ? root : null;
-        // return root;
-    }
-    private TreeNode dfs(TreeNode root){
-        if(root==null) return null;
-        
-        root.left=dfs(root.left);
-        root.right=dfs(root.right);
-         if (root.left == null && root.right == null && root.val == 0) return null;
-        return root;
         
         }
 }
