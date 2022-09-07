8d4021751a8dec692331ec23534f771fbe09fc57
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-08 00:44:35 +0900

    Time: 189 ms (5.01%), Space: 60.7 MB (5.48%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -4,13 +4,13 @@ import java.util.Map;
 import java.util.Optional;
 class Solution {
  public int characterReplacement(String s, int k) {
-        int mx=0,N=s.length(),L=0,R=0,counter=0;
+        int mx=0,N=s.length(),L=0,R=0 ;
         Map<Character,Integer> hm= new HashMap<>();
         char[] sArr=s.toCharArray();
         int maxFreHm=0;
         while (R<N){
             hm.merge(sArr[R],1,Integer::sum);
-             maxFreHm=maxUsingStreamAndMethodReference(hm);
+            maxFreHm=mapMaxVal(hm);
 
             if ( (R-L+1)-maxFreHm>k) {
                 hm.merge(sArr[L],-1,Integer::sum);
@@ -23,7 +23,7 @@ class Solution {
         return  mx;
 
     }
-    private <K, V extends Comparable<V>> V maxUsingStreamAndMethodReference(Map<K, V> map) {
+    private <K, V extends Comparable<V>> V mapMaxVal(Map<K, V> map) {
         Optional<Map.Entry<K, V>> maxEntry = map.entrySet()
                 .stream()
                 .max(Comparator.comparing(Map.Entry::getValue));
