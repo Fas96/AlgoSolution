585a39607dac491dba09242c6df969cdac2605c7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-16 17:16:01 +0900

    Time: 0 ms (100.00%), Space: 48.2 MB (91.77%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -2,13 +2,12 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
      int L=0,mid=0, H=nums.length-1;
         while(L<H){
-            mid = (L + H) >>>1;
-            System.out.println(mid);
+            mid = (L + H) /2;
                 if (nums[mid] == nums[mid ^ 1])
                     L = mid + 1;
                 else
                     H = mid;
-                }
+     }
         return nums[L];
     }
 }
