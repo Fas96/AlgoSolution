a6723105b0b9fd01c4113fb1ce93b2dda2b3cf05
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-16 12:44:10 +0900

    Time: 2524 ms (5.06%), Space: 118.7 MB (70.04%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximumScore(int[] nums, int[] multipliers) {
-        Integer[][] dp= new Integer[1000][1000];
+        int[][] dp= new int[1000][1000];
         return  memo(nums,multipliers,0,0,nums.length-1,dp);
     }
 
-    private int memo(int[] nums, int[] multipliers, int L, int R, int N, Integer[][] dp) {
+    private int memo(int[] nums, int[] multipliers, int L, int R, int N, int[][] dp) {
         if(L == multipliers.length)
             return 0;
-        if(dp[R][L] != null)
+        if(dp[R][L] != 0)
             return dp[R][L];
         int lf = nums[R] * multipliers[L] + memo(nums, multipliers, L + 1, R + 1, N, dp);
         int rg = nums[N] * multipliers[L] + memo(nums, multipliers, L + 1, R, N - 1, dp);
