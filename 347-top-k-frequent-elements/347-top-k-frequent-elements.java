15b08a377703f8a48195c7328d62041b0f7354ad
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-18 20:24:17 +0900

    Time: 29 ms (21.71%), Space: 49.3 MB (83.49%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,14 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+  public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> hm= new HashMap<>();
         int[]res=new int[k];
 
         for (int n : nums) {hm.merge(n,1,Integer::sum);}
-        final int[] cnt = {0};
+         AtomicInteger cnt = new AtomicInteger();
        hm.entrySet().stream().sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).forEach(e->{
-           if(cnt[0] <k){
-           res[cnt[0]++]= (int) e.getKey();}
+           if(cnt.get() <k){
+           res[cnt.getAndIncrement()]= (int) e.getKey();
+           }
        });
+
+
         return res;
      }
 }
