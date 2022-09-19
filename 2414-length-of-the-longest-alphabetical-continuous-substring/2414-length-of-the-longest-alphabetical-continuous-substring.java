7595e03dd3a60fe6ae0bc59fa2c968fd4462afeb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-19 20:26:23 +0900

    Time: 17 ms (33.33%), Space: 54.5 MB (55.56%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestContinuousSubstring(String s) {
-           int start = 0, ans = 1;
+        int start = 0, ans = 1;
         char[] cArr=s.toCharArray();
         for (int i = 1; i < s.length(); ++i) {
             if (cArr[i] != cArr[start] + i - start)
