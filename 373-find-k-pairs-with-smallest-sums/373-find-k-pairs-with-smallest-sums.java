1d0d4e010bc71642f08c639f87c2a2ca1e49d080
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-21 21:22:32 +0900

    Time: 211 ms (11.74%), Space: 125.2 MB (49.68%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-List<List<Integer>> res = new ArrayList<>();
-			PriorityQueue<List<Integer>> pq = new PriorityQueue<>((o1, o2) -> o1.get(0) + o1.get(1) - o2.get(0) - o2.get(1));
 
-			 
-
-			for (int i = 0; i < nums1.length && i < k; i++) {
-				List<Integer> list = Arrays.asList(nums1[i], nums2[0], 0);
-				pq.offer(list);
-			}
+      List<List<Integer>> res = new ArrayList<>();
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((o1, o2) -> o1.get(0) + o1.get(1) - o2.get(0) - o2.get(1));
 
 
-			while (k--> 0 && !pq.isEmpty()) {
-				List<Integer> cur = pq.poll();
-				res.add(Arrays.asList(cur.get(0), cur.get(1)));
-				if(cur.get(2) == nums2.length-1) continue;
-				pq.offer(Arrays.asList(cur.get(0), nums2[cur.get(2) + 1], cur.get(2) + 1));
-			}
 
-			return res;
+        for (int i = 0; i < nums1.length && i < k; i++) {
+            List<Integer> list = Arrays.asList(nums1[i], nums2[0], 0);
+            pq.offer(list);
+        }
+        System.out.println(pq);
+
+        while (k--> 0 && !pq.isEmpty()) {
+            List<Integer> cur = pq.poll();
+            res.add(Arrays.asList(cur.get(0), cur.get(1)));
+            if(cur.get(2) == nums2.length-1) continue;
+            pq.offer(Arrays.asList(cur.get(0), nums2[cur.get(2) + 1], cur.get(2) + 1));
+        }
+
+        return res;
     }
 }
