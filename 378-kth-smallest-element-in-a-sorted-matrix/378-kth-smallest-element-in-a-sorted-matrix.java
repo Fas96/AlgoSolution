fedfdfea02a25346448cfb56bb7f39feb99a4d11
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-09-21 21:32:59 +0900

    Time: 27 ms (39.07%), Space: 47.1 MB (97.47%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,15 +1,16 @@
 class Solution {
-  List<Integer> flatten2DList(List<List<Integer>> listOfLists){
-    return   listOfLists.stream().collect(ArrayList::new, List::addAll, List::addAll);
-  }
-  public int kthSmallest(int[][] matrix, int k) {
-    List<List<Integer>> acc= new ArrayList<>();
-    for (int[] l : matrix) {
-      acc.add( Arrays.stream(l).boxed().collect(Collectors.toList()));
-    }
-    List<Integer> flatten2DList = flatten2DList(acc).stream().sorted().collect(Collectors.toList());
+    public int kthSmallest(int[][] matrix, int k) {
+      int ans=0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o1-o2);
 
- 
-    return  flatten2DList.get(k-1);
-  }
+        for (int []a:matrix) {
+            for (int n : a) {
+                pq.add(n);
+            }
+        }
+        while (k-->0){
+            ans=pq.poll();
+        }
+        return ans;  
+    }
 }
