54c8cd9c02e1eed1266055c9e42b4b7a93942978
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-10-18 12:03:04 +0900

    Time: 106 ms (88.00%), Space: 45.3 MB (56.00%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.ts
@@ -0,0 +1,23 @@
+function compress(chars: string[]): number {
+    let compressed = "";
+    let count = 1;
+    for (let i = 1; i < chars.length; i++) {
+        if (chars[i] == chars[i - 1]) {
+            count++;
+        } else {
+            compressed += chars[i - 1];
+            if (count > 1) {
+                compressed += count;
+            }
+            count = 1;
+        }
+    }
+    compressed += chars[chars.length - 1];
+    if (count > 1) {
+        compressed += count;
+    }
+    for (let i = 0; i < compressed.length; i++) {
+        chars[i] = compressed[i];
+    }
+    return compressed.length;
+};
