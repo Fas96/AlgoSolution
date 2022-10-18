41d2174d905925b93696659f9c720c85016dcfda
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-10-19 08:49:44 +0900

    Time: 185 ms (50.00%), Space: 53 MB (100.00%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.ts
+++ 0565-array-nesting/0565-array-nesting.ts
@@ -1,15 +1,33 @@
 function arrayNesting(nums: number[]): number {
-let max = 0;
+    // let max = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     let count = 0;
+    //     let j = i;
+    //     //checking the sets that can be stacked over each other by the index
+    //     while (nums[j] != -1) {
+    //         let temp = nums[j];
+    //         //we made nums[j] -1 so that we dont visit it again in the loop
+    //         nums[j] = -1;
+    //         //j goes to the next index if it was not already visited
+    //         j = temp;
+    //         //count if the loop continues
+    //         count++; 
+    //     }
+    //     max = Math.max(max, count);
+    // }
+    // return max;
+      let mx=0;
     for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-        let j = i;
-        while (nums[j] != -1) {
-            let temp = nums[j];
-            nums[j] = -1;
-            j = temp;
-            count++;
+        let cnt=0;
+        let j=i;
+        while (nums[j]!=-1){
+            let temp=nums[j]
+            nums[j]=-1
+            j=temp;
+            cnt++;
         }
-        max = Math.max(max, count);
+        mx=Math.max(mx,cnt)
     }
-    return max;
+    
+    return mx;
 };
