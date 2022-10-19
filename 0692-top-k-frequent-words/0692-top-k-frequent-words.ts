8c90f096c08913bd0709cecacfda2424c66b72e0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-10-19 13:38:25 +0900

    Time: 137 ms (45.35%), Space: 47.6 MB (62.79%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.ts
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.ts
@@ -1,11 +1,15 @@
 
 function topKFrequent(words: string[], k: number): string[] {
    let result: string[] = [];
+    
     let map: Map<string, number> = new Map();
+    
     for (let word of words) {
         map.set(word, (map.get(word) || 0) + 1);
     }
+    
     let wordsA: string[] = Array.from(map.keys());
+    
     wordsA.sort((w1, w2) => map.get(w1) == map.get(w2) ? w1.localeCompare(w2) : map.get(w2) - map.get(w1));
     
     for (let i = 0; i < k; i++) {
