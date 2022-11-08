08af49d7fba56f745b7e918dc5a28fc1587b7feb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-08 09:07:30 +0900

    Time: 3 ms (93.08%), Space: 42.4 MB (75.61%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,13 +1,16 @@
 class Solution {
     public String makeGood(String s) {
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(sb.length()>0&&Math.abs(s.charAt(i)-sb.charAt(sb.length()-1))==32){
-                sb.deleteCharAt(sb.length()-1);
-            }else{
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
+  while (true) {
+         boolean flag = false;
+         for (int i = 0; i < s.length() - 1; i++) {
+             if (Math.abs(s.charAt(i) - s.charAt(i + 1)) == 32) {
+                 s = s.substring(0, i) + s.substring(i + 2);
+                 flag = true;
+                 break;
+             }
+         }
+         if (!flag) break;
+     }
+        return s;
     }
 }
