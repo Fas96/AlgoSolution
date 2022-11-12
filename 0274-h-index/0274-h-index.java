914d983f15edeef9009e805bcc31140909fafb59
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-12 13:18:54 +0900

    Time: 4 ms (44.12%), Space: 42.8 MB (6.47%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,16 +1,16 @@
+
+import static java.util.Arrays.sort;
 class Solution {
     public int hIndex(int[] citations) {
+         Arrays.sort(citations);
         int N = citations.length;
-        int[] count = new int[N+1];
-        for (int c : citations) {
-            if (c >= N) count[N]++;
-            else count[c]++;
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            if (citations[i] >= N - i) {
+                res = N - i;
+                break;
+            }
         }
-        int sum = 0;
-        for (int i = N; i >= 0; i--) {
-            sum += count[i];
-            if (sum >= i) return i;
-        }
-        return 0;
+        return res;
     }
 }
