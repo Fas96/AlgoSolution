5d78428e5a8c858f7820afefdf74370821d644d0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-14 11:26:22 +0900

    Time: 51 ms (52.64%), Space: 49 MB (70.73%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+         
+        int N=stones.length,res=0; 
+        int[] parent=new int[N];
+        for(int i=0;i<N;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<N;j++){
+                if(stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1]){
+                    union(parent,i,j);
+                }
+            }
+        }
+        
+        for(int i=0;i<N;i++){
+            if(parent[i]==i){
+                res++;
+            }
+        }
+        return N-res;
+
+    }
+
+    private void union(int[] parent, int i, int j) {
+        int p1=find(parent,i);
+        int p2=find(parent,j);
+        if(p1!=p2){
+            parent[p1]=p2;
+        }
+    }
+
+    private int find(int[] parent, int i) {
+        if(parent[i]!=i){
+            parent[i]=find(parent,parent[i]);
+        }
+        return parent[i];
+    }
+}
