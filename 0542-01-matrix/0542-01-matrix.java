ed26e9088ba55fbe40e873bc5371d6cc04a95353
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-21 21:10:27 +0900

    Time: 33 ms (47.06%), Space: 73.3 MB (34.33%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+          int m = mat.length, n = mat[0].length;
+        int[][] res = new int[m][n];
+         //bfs
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new int[]{i, j});
+                } else {
+                    res[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] dir : dirs) {
+                int x = cur[0] + dir[0], y = cur[1] + dir[1];
+                if (x < 0 || x >= m || y < 0 || y >= n || res[x][y] <= res[cur[0]][cur[1]] + 1) continue;
+                res[x][y] = res[cur[0]][cur[1]] + 1;
+                q.offer(new int[]{x, y});
+            }
+        }
+        
+        
+        return res;
+    }
+}
