08f144db8a0871fc4cf7bf6158737bbe5c359ff3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-23 09:36:08 +0900

    Time: 15 ms (58.49%), Space: 41.8 MB (40.53%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+         int N = board[0].length;
+        solveSudoku(board,N);
+    }
+    public boolean solveSudoku(char[][] board,int N) {
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (board[i][j]=='.'){
+          for (char tryNum = '1'; tryNum <= '9'; tryNum++) {
+            if(isValidPlacement(board,tryNum,i,j)){
+              board[i][j]= tryNum;
+              if(solveSudoku(board,N)){
+                return true;
+              }else{
+                board[i][j]='.';
+              }
+            }
+          }
+          return false;
+
+        }
+      }
+    }
+    return true;
+  }
+
+  private boolean isNumInRow(char[][] board,char number,int row){
+    for (int i = 0; i < board.length; i++) {
+      if(board[row][i]==number)return true;
+    }
+    return false;
+  }
+  private boolean isNumInColumn(char[][] board,char number,int col){
+    for (int i = 0; i < board.length; i++) {
+      if(board[i][col]==number)return true;
+    }
+    return false;
+  }
+  private boolean isNumInBox(char[][] board,char number,int row,int col){
+    int localRow=row-row%3;
+    int localCol=col-col%3;
+
+    for (int i = localRow; i < localRow+3; i++) {
+      for (int j = localCol; j < localCol+3; j++) {
+        if(board[i][j]==number)return true;
+      }
+    }
+    return false;
+  }
+  private boolean isValidPlacement(char[][] board,char number,int row,int col){
+    return !isNumInRow(board, number,row) &&
+        !isNumInColumn( board, number, col)&&
+      !isNumInBox(board,number,row,col);
+  }
+}
