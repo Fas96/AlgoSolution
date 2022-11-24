516a9ee36e916cead20542e2e354907c83900621
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-11-24 12:55:57 +0900

    Time: 177 ms (60.09%), Space: 43.2 MB (18.06%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -10,15 +10,23 @@ class Solution {
 
     private boolean search(char[][] board, String word, int i, int j, int index) {
         if (index == word.length()) return true;
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) return false;
+        if (isBoundaryCharCheck(board, word, i, j, index)) return false;
         char temp = board[i][j];
         board[i][j] = ' ';
-        boolean found = search(board, word, i + 1, j, index + 1) ||
+        boolean found = isFoundInAnyDirections(board, word, i, j, index);
+        board[i][j] = temp;
+        return found;
+    }
+
+    private boolean isFoundInAnyDirections(char[][] board, String word, int i, int j, int index) {
+        return search(board, word, i + 1, j, index + 1) ||
                 search(board, word, i - 1, j, index + 1) ||
                 search(board, word, i, j + 1, index + 1) ||
                 search(board, word, i, j - 1, index + 1);
-        board[i][j] = temp;
-        return found;
-    } 
+    }
+
+    private static boolean isBoundaryCharCheck(char[][] board, String word, int i, int j, int index) {
+        return i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index);
+    }
 
 }    
