5d4b6bba0e9bafce9b3150fa95013634af8684bd
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-12-05 21:12:57 +0900

    Time: 93 ms (76.23%), Space: 59.2 MB (90.58%) - LeetHub

--- 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.java
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.java
@@ -1,26 +1,25 @@
 class Solution {
     public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
-      int n = nums1.length;
+         int MOD = 1000000007;
+        int absSum = 0,N = nums1.length; 
+        int maxDiff = 0;
         int[] sorted = nums1.clone();
         Arrays.sort(sorted);
-        int MOD = 1000000007;
-        int sum = 0;
-        int maxDiff = 0;
-        for (int i = 0; i < n; i++) {
-            int diff = Math.abs(nums1[i] - nums2[i]);
-            sum = (sum + diff) % MOD;
+
+        for (int i = 0; i < N; i++) {
+            int diff = Math.abs(nums1[i] - nums2[i]); 
+            absSum = (absSum + diff) % MOD;
             int j = binarySearch(sorted, nums2[i]);
-            if (j < n) {
+            if (j < N) {
                 maxDiff =  Math.max(maxDiff, diff - (sorted[j] - nums2[i])) ;
             }
             if (j > 0) {
                 maxDiff = Math.max(maxDiff, diff - (nums2[i] - sorted[j - 1]));
             }
         }
-        return (sum - maxDiff + MOD) % MOD;
 
+       return (absSum - maxDiff +MOD) % MOD;
     }
-
     public int binarySearch(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
         if (target > nums[high]) {
