d8dbb03dbad1f5baee5bb2a584e925aa8b2fafb1
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2022-12-30 22:48:24 +0900

    Time: 5 ms (46.49%), Space: 44.5 MB (73.42%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        HashMap<Integer,List<Integer>> edgeNConnections = new HashMap<>();
+        int n = graph.length;
+        for (int i = 0; i < n; i++) {
+            edgeNConnections.computeIfAbsent(i, f -> new ArrayList<>());
+            for (int j = 0; j < graph[i].length; j++) {
+                edgeNConnections.get(i).add(graph[i][j]);
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(0, n - 1, edgeNConnections, path, res);
+        System.out.println(edgeNConnections);
+
+        return res;
+    }
+
+    private void dfs(int i, int i1, HashMap<Integer, List<Integer>> edgeNConnections, List<Integer> path, List<List<Integer>> res) {
+        if (i == i1) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int j = 0; j < edgeNConnections.get(i).size(); j++) {
+            path.add(edgeNConnections.get(i).get(j));
+            dfs(edgeNConnections.get(i).get(j), i1, edgeNConnections, path, res);
+            path.remove(path.size() - 1);
+        }
+
+    }
+}
