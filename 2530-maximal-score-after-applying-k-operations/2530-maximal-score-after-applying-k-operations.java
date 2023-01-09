d22a354f5662a7d29dfbde1dd9e97226ef45d8d4
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-01-09 21:33:38 +0900

    Time: 192 ms (80.00%), Space: 59.8 MB (40.00%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -1,54 +1,34 @@
 class Solution {
     public long maxKelements(int[] nums, int k) { 
-       
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+   long ans= 0; 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         for(int i = 0; i < nums.length; i++){
-            pq.add(-nums[i]);
+            pq.add(nums[i]);
         }
-        long ans= 0;
-        while (k > 0 ){
-            int cur = -pq.poll();
+        while (k > 0 && !pq.isEmpty()){
+            int cur = pq.poll();
             ans += cur;
-            cur = (cur+2)/3;
-            pq.add(-cur);
+            pq.add((cur+2)/3);
             k--;
         }
-         
+
         return ans;
-//        int n = nums.length;
-//        int maxKScore = 0;
-//        int count = 0;
-//
-//
-//        for (int i = 0; i < n; i++) {
-//           int index=  binarySearchMaxIndex(nums);
-//            maxKScore += nums[index];
-//            System.out.println(Arrays.toString(nums));
-//            System.out.println("maxKScore = " + maxKScore+" index = "+index);
-//           nums[index] = ceilF(nums[index]);
-//              count++;
-//              if (count == k) break;
-//        }
-//        return maxKScore;
     }
 
-    private int binarySearchMaxIndex(int[] nums) {
-        int n = nums.length;
-        int l = 0;
-        int r = n - 1;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < nums[mid + 1]) {
-                l = mid + 1;
-            } else {
-                r = mid;
+    private int bSearch(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
             }
         }
-        return l;
+        return index;
     }
-
+ 
 
     private int ceilF(int num) {
-     return (int) Math.ceil((double) num / 3);
+         return (int) Math.ceil((double) num / 3);
     }
 }
