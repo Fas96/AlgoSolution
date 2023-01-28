fabba095d3a51ea277f0c1e9da76c35a0e2143de
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-01-28 11:31:46 +0900

    Time: 103 ms (44.54%), Space: 70.2 MB (26.89%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -1,23 +1,49 @@
- class SummaryRanges {
+//Personal Understanding refactored copy 
+class SummaryRanges {
+
         TreeMap<Integer, Integer> intervals;
         public SummaryRanges() {
             intervals = new TreeMap<>();
         }
         public void addNum(int value) {
-            if(intervals.containsKey(value)) return;
+            if(isValidInKeys(value)) return;
+            extractLowHigh(value);
+        }
+
+        private void extractLowHigh(int value) {
             Integer low = intervals.lowerKey(value);
             Integer high = intervals.higherKey(value);
-            if(low != null && high != null && intervals.get(low) + 1 == value && high == value + 1){
+            insertOnConditions(value, low, high);
+        }
+
+        private void insertOnConditions(int value, Integer low, Integer high) {
+            if(isValidRange(value, low, high)){
                 intervals.put(low, intervals.get(high));
                 intervals.remove(high);
-            } else if(low != null && intervals.get(low) + 1 >= value){
+            } else if(isHigherBoundsMissing(value, low)){
                 intervals.put(low, Math.max(intervals.get(low), value));
-            } else if(high != null && high == value + 1){
+            } else if(isLowerBoundMissing(value, high)){
                 intervals.put(value, intervals.get(high));
                 intervals.remove(high);
             } else {
                 intervals.put(value, value);
             }
+        }
+
+        private boolean isValidInKeys(int value) {
+            return intervals.containsKey(value);
+        }
+
+        private boolean isHigherBoundsMissing(int value, Integer low) {
+            return low != null && intervals.get(low) + 1 >= value;
+        }
+
+        private boolean isLowerBoundMissing(int value, Integer high) {
+            return high != null && high == value + 1;
+        }
+
+        private boolean isValidRange(int value, Integer low, Integer high) {
+            return low != null && high != null && intervals.get(low) + 1 == value && high == value + 1;
         }
 
         public int[][] getIntervals() {
