0c6e0d992a40d6ff1421df306ce13d8a3b919831
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-02 09:23:13 +0900

    Time: 3 ms (13.41%), Space: 41.1 MB (71.33%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+         int n = words.length; 
+        List<Character> orderList = order.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        for (int i = 0; i < n-1; i++) {
+            if(!isAlienSorted(words[i], words[i+1], orderList)) return false;
+        }
+        return true;
+    }
+    private boolean isAlienSorted(String word1, String word2, List<Character> orderList) {
+        int n = word1.length();
+        int m = word2.length();
+        int i = 0;
+        while(i < n && i < m) {
+            if(word1.charAt(i) != word2.charAt(i)) {
+                return orderList.indexOf(word1.charAt(i)) < orderList.indexOf(word2.charAt(i));
+            }
+            i++;
+        }
+        return n <= m;
+    }
+    
+}
