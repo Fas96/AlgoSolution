9ed390e4700c64537d6d11445e1869f3d67e7878
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-05 16:53:38 +0900

    Time: 1086 ms (10.40%), Space: 43.5 MB (51.03%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,26 +4,19 @@ class Solution {
         int m = p.length();
 
         int[] pMap = new int[26];
-        int[] sMap = new int[26];
-//           Map<Character,Integer> sMap = new HashMap<>();
-//           Map<Character,Integer> pMap = new HashMap<>();
+        int[] sMap = new int[26]; 
 
         for (Character c : p.toCharArray()) {
-            pMap[c - 'a']++;
-//            pMap.merge(c, 1, Integer::sum);
+            pMap[c - 'a']++; 
         }
 
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i <= n - m; i++) {
-             // String sub = s.substring(i, i+n);
+        for (int i = 0; i <= n - m; i++) { 
             for (int j = 0; j < m; j++) {
-                sMap[s.charAt(i + j) - 'a']++;
-//                 sMap.merge(sub.charAt(j), 1, Integer::sum);
+                sMap[s.charAt(i + j) - 'a']++; 
             }
             if (Arrays.equals(pMap, sMap))ans.add(i);
-            Arrays.fill(sMap, 0);
-//            if(sMap.equals(pMap)) ans.add(i);
-//             sMap.clear();
+            Arrays.fill(sMap, 0); 
         }
         return ans;
     }
