f1af520a0bb55af4712cb0bfbd93967f7df44493
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-12 01:01:44 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (88.01%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,13 +14,18 @@
  * }
  */
 class Solution {
+      TreeNode prev = null;
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
-    }
-     private boolean isValidBST(TreeNode root, Integer min, Integer max) {
             if (root == null) return true;
-            if (min != null && root.val <= min) return false;
-            if (max != null && root.val >= max) return false;
-            return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
-        }
+            if (!isValidBST(root.left)) return false;
+            if (prev != null && prev.val >= root.val) return false;
+            prev = root;
+            return isValidBST(root.right);
+    }
+     // private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+     //        if (root == null) return true;
+     //        if (min != null && root.val <= min) return false;
+     //        if (max != null && root.val >= max) return false;
+     //        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+     //    }
 }
