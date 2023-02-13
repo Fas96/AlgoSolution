ceb482e8381efed1b21d76d7e1b80b6f3b4c704a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-13 20:28:45 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (52.67%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,11 +1,16 @@
 class Solution {
     public int balancedStringSplit(String s) {
          int ans = 0;
-        int count = 0;
+      
+        int LCount=0, RCount=0;
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'L') count++;
-            else count--;
-            if(count == 0) ans++;
+            if(s.charAt(i) == 'L') LCount++;
+            else RCount++;
+            if(LCount == RCount) {
+                ans++;
+                LCount = 0;
+                RCount = 0;
+            }
         }
         return ans;
     }
