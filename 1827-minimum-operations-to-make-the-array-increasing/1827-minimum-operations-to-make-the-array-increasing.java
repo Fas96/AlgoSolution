7c620e637db91dc78d8ea3438c861ae6702ea892
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-13 21:12:39 +0900

    Time: 2 ms (93.97%), Space: 42.7 MB (60.43%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -3,7 +3,7 @@ class Solution {
          int n= nums.length;
         int ans=0;
         for (int i = 1; i < n; i++) {
-            if (nums[i]<=nums[i-1]){
+            if(nums[i-1]>=nums[i]){
                 ans+=nums[i-1]-nums[i]+1;
                 nums[i]=nums[i-1]+1;
             }
