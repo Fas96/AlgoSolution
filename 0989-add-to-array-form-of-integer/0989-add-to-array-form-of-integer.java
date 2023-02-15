84714541b36c73e83811812e3c00e426582fbc6a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-15 09:24:58 +0900

    Time: 109 ms (7.43%), Space: 43.6 MB (85.09%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,18 +1,15 @@
 import java.math.BigInteger;
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-          StringBuilder s = new StringBuilder();
-        for (int i = 0; i < num.length; i++) {
-            s.append(num[i]);
-        }
-        BigInteger n = new BigInteger(s.toString());
-        BigInteger k1 = new BigInteger(String.valueOf(k));
-        n = n.add(k1);
+        StringBuilder s = new StringBuilder();
+ 
+        for (int i : num) s.append(i);
+        
+        BigInteger n = (new BigInteger(s.toString())).add(new BigInteger(String.valueOf(k)));
+        
         String s1 = String.valueOf(n);
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < s1.length(); i++) {
-            list.add(Integer.parseInt(String.valueOf(s1.charAt(i))));
-        }
-        return list;
+ 
+       return s1.chars().mapToObj(c -> c - '0').collect(Collectors.toList());
+ 
     }
 }
