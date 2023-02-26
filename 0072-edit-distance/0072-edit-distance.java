0fefb5065a8aa583a1b22317b071c659c13a64eb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-26 11:12:30 +0900

    Time: 6 ms (67.73%), Space: 42.5 MB (41.57%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minDistance(String word1, String word2) { 
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (isSameCharacter(word1, word2, i, j)) diagonalValue(dp, i, j);
+                else minOfTDL(dp, i, j);
+            }
+        }
+        return dp[m][n];
+    }
+
+    private static void minOfTDL(int[][] dp, int i, int j) {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+    }
+
+    private static void diagonalValue(int[][] dp, int i, int j) {
+        dp[i][j] = dp[i - 1][j - 1];
+    }
+
+    private static boolean isSameCharacter(String word1, String word2, int i, int j) {
+        return word1.charAt(i - 1) == word2.charAt(j - 1);
+    }
+}
