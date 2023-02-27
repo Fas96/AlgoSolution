93b8482b1849634716a1e262252ee8924a9ea2fe
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-27 09:32:26 +0900

    Time: 1 ms (71.09%), Space: 42.2 MB (85.42%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -0,0 +1,58 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+};
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+         return construct(grid, 0, 0, grid.length);
+    }
+
+    private Node construct(int[][] grid, int gridX, int gridY, int length) {
+         if (length == 1) return new Node(grid[gridX][gridY] == 1, true);
+        int half = length / 2;
+        Node topLeft = construct(grid, gridX, gridY, half);
+        Node topRight = construct(grid, gridX, gridY + half, half);
+        Node bottomLeft = construct(grid, gridX + half, gridY, half);
+        Node bottomRight = construct(grid, gridX + half, gridY + half, half);
+        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf && topLeft.val == topRight.val && topRight.val == bottomLeft.val && bottomLeft.val == bottomRight.val) {
+            return new Node(topLeft.val, true);
+        }
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    };
+}
