312c1a63b276efb1d3d950ab6bd0dac6d8e198cb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-02-28 09:21:19 +0900

    Time: 23 ms (37.38%), Space: 52.3 MB (25.72%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     Map<String, Integer> map = new HashMap<>();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        if(root == null) return null;
+        List<TreeNode> ans = new LinkedList<>();
+        postOrderTraversal(root, ans);
+        return ans;
+
+    }
+
+    private String postOrderTraversal(TreeNode root, List<TreeNode> ans) {
+        if(root == null) return "FAS";
+        String subTree = postOrderTraversal(root.left, ans) + "," +  postOrderTraversal(root.right, ans) + "," + root.val;
+        map.put(subTree, map.getOrDefault(subTree, 0) + 1);
+        if(map.get(subTree) == 2) ans.add(root);
+        return subTree;
+    }
+}
