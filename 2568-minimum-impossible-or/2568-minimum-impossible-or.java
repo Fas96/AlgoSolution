32089df4405ba4f2dd9ea225c4a23529cb096b22
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-03-06 21:23:42 +0900

    Time: 1 ms (100.00%), Space: 57.8 MB (37.34%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.java
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.java
@@ -1,15 +1,43 @@
 class Solution {
     public int minImpossibleOR(int[] nums) {
-         Set<Integer> list = new HashSet<>();
-       
-        for (int nn:nums) list.add(nn);
-        
-        int p=1;
-     
-        for(int i=1;list.contains(p) && i<100000;i++){
-            p=(int)Math.pow(2,i);
+           int y=0;
+          for (int nb :nums){
+            if((nb & (nb-1))==0){y|=nb;}
         }
-          
-        return (int) p;
+        for(int i=0;i<30;i++){
+            if((y>>i & 1)==0)return 1<<i;
+        }
+        
+        return 1<<30;
+        
     }
 }
+/*    
+        001
+        010
+        011
+        100
+        101
+        110
+        111
+        
+
+        int n = nums.length;
+        int nonExperssable = -1;
+        Set<Integer> list = new HashSet<>();
+        for (int nb :nums)list.add(nb);
+        for (int i = 0; i < n-1; i++) {
+            list.add(nums[i]| nums[i+1]);
+        }
+
+        Collections.sort(new ArrayList<>(list));
+        
+        for (int i = 1; i < 100000; i++) { 
+            if(!list.contains(i))return i;
+        }
+
+        return nonExperssable;
+*/
+//[1,25,2,72]
+//2,3,4,
+//2,
