cfddf396914aad2ac002fe17782857d8d7eeacd6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-03-13 20:25:43 +0900

    Time: 1 ms (53.61%), Space: 40.8 MB (18.25%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public int minimumMoves(String s) {
        
-        int nOperations = 0;
-       int idx = 0;
-       int N = s.length();
-         while (idx < N) {
-              if (s.charAt(idx) == 'X') {
+        int nOperations = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'X'){ 
                 nOperations++;
-                idx += 3;
-              } else  idx++;
-              
-         }
-    return nOperations;
+                i += 2;
+            }
+            
+        }
+        return nOperations;
     }
 }
