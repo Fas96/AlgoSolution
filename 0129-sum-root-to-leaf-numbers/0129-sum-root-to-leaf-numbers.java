4bf59d3b6459f5a3bb10452cfa7bdb2303252111
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-03-14 09:23:42 +0900

    Time: 1 ms (29.99%), Space: 40.4 MB (24.48%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,17 +14,32 @@
  * }
  */
 class Solution {
+     List<List<Integer>> pathValues = new ArrayList<>();
     public int sumNumbers(TreeNode root) {
-        
-      if (root == null) return 0;
-        return sumNumbers(root, 0);
+        if (root == null) return 0;
+        List<Integer> path = new ArrayList<>();
+        dfs(root, path);
+       
+        int sum = 0;
+        for (List<Integer> pathValue : pathValues) {
+            int pathSum = 0;
+            for (int i = 0; i < pathValue.size(); i++) {
+                pathSum += pathValue.get(i) * Math.pow(10, pathValue.size() - i - 1);
+            }
+            sum += pathSum;
+        }
+        return sum;
 
     }
 
-    private int sumNumbers(TreeNode root, int sum) {
-        if (root == null) return 0;
-        sum = sum * 10 + root.val;
-        if (root.left == null && root.right == null) return sum;
-        return sumNumbers(root.left, sum) + sumNumbers(root.right, sum);
+    private void dfs(TreeNode root, List<Integer> path) {
+        if (root == null) return;
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            pathValues.add(new ArrayList<>(path));
+        }
+        dfs(root.left, path);
+        dfs(root.right, path);
+        path.remove(path.size() - 1);
     }
 }
