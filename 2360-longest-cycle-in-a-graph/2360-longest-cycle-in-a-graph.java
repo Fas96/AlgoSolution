d4419b59db968551a11f22124e24dc5ea991a79b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-03-26 23:49:41 +0900

    Time: 163 ms (27.41%), Space: 86.8 MB (35.14%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -0,0 +1,90 @@
+class Solution {
+   public int longestCycle(int[] edges) {
+
+        int n = edges.length;
+        
+        int[] inDegree = new int[n]; 
+
+        ArrayList <Integer>[] adjList = new ArrayList[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            adjList[i] = new ArrayList <Integer>();
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            int u = i;
+            int v = edges[i];      
+
+            if(v != - 1)  
+            {
+                adjList[u].add(v); 
+                inDegree[v] += 1;  
+            }
+        }
+
+        Queue <Integer> q = new LinkedList<>();
+        int cycle = -1;
+        boolean[] vis = new boolean[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            if(inDegree[i] == 0) {
+                q.add(i);
+                vis[i] = true; 
+            }
+        }
+
+        while(q.size() > 0) 
+        {
+            int u = q.poll();
+
+            for(int i = 0; i < adjList[u].size(); i++)
+            {
+                int v = adjList[u].get(i); 
+                inDegree[v] -= 1;  
+
+                if(inDegree[v] == 0)
+                {
+                    q.add(v); 
+                    vis[v] = true;  
+                }
+            }
+        }
+      
+        for(int i = 0; i < n; i++)
+        { 
+            if(!vis[i]) 
+            {
+                cycle = Math.max(cycle, dfs(adjList, vis, i));  
+            }   
+        }
+
+        return cycle; 
+    }
+
+    public int dfs(ArrayList <Integer>[] adjList, boolean[] vis, int node)
+    {
+
+        if(vis[node])
+        {
+            return  0;
+        }
+
+        int nodeCount = 1;
+        vis[node] = true;
+
+        for(int i = 0; i < adjList[node].size(); i++)
+        {
+            int v = adjList[node].get(i);
+
+            if(vis[v] == false)
+            {
+                nodeCount += dfs(adjList, vis, v);
+            }
+        }
+
+        return nodeCount;
+    }
+}
