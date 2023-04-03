5534d4966a98d37c4c5ff197306d6954252af1ca
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-03 09:17:43 +0900

    Time: 17 ms (98.31%), Space: 51.2 MB (15.64%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+          Arrays.sort(people);
+        int result = 0;
+        int n = people.length;
+        int i = 0, j = n - 1;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            result++;
+        }
+        return result;
+    }
+}
