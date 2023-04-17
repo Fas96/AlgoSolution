2cd2e29ed954d424b847f042fb89472058be2c66
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-17 09:10:55 +0900

    Time: 4 ms (13.79%), Space: 43 MB (13.43%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,13 +1,6 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < candies.length; i++) {
-            max = Math.max(max, candies[i]);
-        }
-        List<Boolean> ans = new ArrayList<>();
-        for (int i = 0; i < candies.length; i++) {
-            ans.add(candies[i] + extraCandies >= max);
-        }
-        return ans;
+         int max = Arrays.stream(candies).max().getAsInt();
+       return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= max).collect(Collectors.toList());
     }
 }
