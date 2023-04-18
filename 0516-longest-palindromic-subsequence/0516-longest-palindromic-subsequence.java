f64f5c0f3ba7563334054f4e1671466dbcdb4b48
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-18 21:10:57 +0900

    Time: 53 ms (51.92%), Space: 52.2 MB (21.75%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,24 +1,18 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-   int[][] dp = new int[s.length()][s.length()];
-        return subseq(s, 0, s.length() - 1, dp);
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
-    public int subseq(String s, int i, int j, int[][] dp) {
-        if (i == j) {
-            return 1;
-        }
-        if (i > j) {
-            return 0;
-        }
-        if (dp[i][j] != 0) {
-            return dp[i][j];
-        }
-        int ans = 0;
-        if (s.charAt(i) == s.charAt(j)) {
-            ans = 2 + subseq(s, i + 1, j - 1, dp);
-        } else {
-            ans = Math.max(subseq(s, i + 1, j, dp), subseq(s, i, j - 1, dp));
-        }
-        return dp[i][j] = ans;
-    }
+     
 }
