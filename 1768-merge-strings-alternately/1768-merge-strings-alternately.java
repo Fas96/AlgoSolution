f7eb0f8e8196b273075a1b99aa935fa78b21df37
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-18 09:25:14 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (88.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -6,24 +6,12 @@ class Solution {
         StringBuilder sb= new StringBuilder();
         int i=0;
         while (i<minLen){
-             sb.append(word1.charAt(i));
-             sb.append(word2.charAt(i));
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
             i++;
         }
-        int ffl=fl-minLen;
-        int ssl=sl-minLen;
-        if(ffl>-1){
-            for (int j = i; j < fl; j++) {
-                sb.append(word1.charAt(j));
-                
-            }
-        }
-        if(ssl>-1){
-            for (int j = i; j < sl; j++) {
-                sb.append(word2.charAt(j));
-
-            }
-        }
+        sb.append(word1.substring(minLen));
+        sb.append(word2.substring(minLen));
 
 
         return sb.toString();
