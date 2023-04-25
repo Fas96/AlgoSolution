30f4d904473279e5bcee7d53d3a9fef73f0e2fe5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-25 21:06:47 +0900

    Time: 11 ms (75.42%), Space: 42.5 MB (33.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,8 +2,9 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
          TreeSet<Integer> set=new TreeSet<>();
         for (int num : nums) {
-            Integer ceiling = set.ceiling(num);
-            System.out.print(ceiling);
+            
+            Integer ceiling = set.ceiling(num); 
+            
             if(ceiling!=null)set.remove(ceiling);
             
             set.add(num);
