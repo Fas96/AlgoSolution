b563764adba4d79bad4cc83612ddcbefd3d0b116
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-04-28 09:18:13 +0900

    Time: 36 ms (45.80%), Space: 41.8 MB (93.51%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numSimilarGroups(String[] strs) {
+   Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+             map.put(str, null);
+        }
+        int ans = 0;
+        for (String str : strs) {
+            if (map.get(str) == null) {
+                ans++;
+                dfs(str, map);
+            }
+        }
+        return ans;
+
+    }
+
+    private void dfs(String str, Map<String, List<String>> map) {
+        if (map.get(str) != null) {
+            return;
+        }
+        map.put(str, new ArrayList<>());
+        for (String key : map.keySet()) {
+            if (isSimilar(str, key)) {
+                dfs(key, map);
+            }
+        }
+    }
+
+    private boolean isSimilar(String str, String key) {
+           int diff = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != key.charAt(i)) {
+                    diff++;
+                }
+                if (diff > 2) {
+                    return false;
+                }
+            }
+            return true;
+    }
+}
