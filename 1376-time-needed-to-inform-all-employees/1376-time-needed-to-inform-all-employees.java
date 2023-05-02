8999190afbb99928a2a76b1650998a4a5143bc6e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-02 21:30:30 +0900

    Time: 426 ms (5.21%), Space: 50.8 MB (99.08%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+     int ans=0;
+        for (int i = 0; i < n; i++) {
+            if(informTime[i]==0){
+                ans=Math.max(ans,dfs(i,manager,informTime));
+            }
+        }
+        return ans;
+
+    }
+
+    private int dfs(int idx, int[] manager, int[] informTime) {
+        if(manager[idx]==-1){
+            return 0;
+        }
+        return informTime[manager[idx]]+dfs(manager[idx],manager,informTime);
+    }
+}
