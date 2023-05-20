be05a7928a0ebc9601a6586c39298a4df39647b2
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-21 00:18:37 +0900

    Time: 21 ms (56.25%), Space: 40 MB (78.47%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+   for(int i=0;i<nums.length;i++){
+            if(isCycle(nums,i)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private boolean isCycle(int[] nums, int i) {
+        boolean isForward=nums[i]>=0;
+        int slow=i,fast=i;
+        do{
+            slow=getNextIndex(nums,isForward,slow);
+            fast=getNextIndex(nums,isForward,fast);
+            if(fast!=-1){
+                fast=getNextIndex(nums,isForward,fast);
+            }
+        }while(slow!=-1&&fast!=-1&&slow!=fast);
+        return slow!=-1&&slow==fast;
+    }
+
+    private int getNextIndex(int[] nums, boolean isForward, int slow) {
+        boolean direction=nums[slow]>=0;
+        
+        if(isForward!=direction)return -1;
+        
+        int nextIndex=(slow+nums[slow])%nums.length;
+        
+        if(nextIndex<0)nextIndex+=nums.length;
+        if(nextIndex==slow)return -1;
+        
+        return nextIndex;
+    }
+}
