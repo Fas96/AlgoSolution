3ee6ed17638139ddad0854359bf177270fae910d
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-22 09:14:39 +0900

    Time: 18 ms (16.84%), Space: 48 MB (5.00%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,7 +4,8 @@ class Solution {
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+       PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(map::get));
+        
         for(int key : map.keySet()){
             pq.offer(key);
             if(pq.size() > k) pq.poll();
