18766269d694082c18961bde0431fd67751761f1
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-27 18:23:25 +0900

    Time: 4 ms (12.89%), Space: 43.9 MB (98.86%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,35 +1,47 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-         List<List<Integer>> list = new ArrayList<>();
-        int start = newInterval[0];
-        int end = newInterval[1];
-
-        boolean isInserted = false;
-
-        for(int[] x: intervals){
-            if(isInserted){
-                list.add(Arrays.asList(x[0],x[1]));
-            } else if(end < x[0]){
-                list.add(Arrays.asList(start, end));
-                list.add(Arrays.asList(x[0],x[1]));
-                isInserted = true;
-            } else if ( start > x[1]) {
-                list.add(Arrays.asList(x[0],x[1]));
-            } else {
-                start = Math.min(start, x[0]);
-                end = Math.max(end, x[1]);
+        
+   
+        if(intervals==null||intervals.length==0) return new int[][]{newInterval};
+        if(newInterval==null) return intervals;
+        
+        List<Interval> interVals=new ArrayList<>();
+        for(int[] interval:intervals){
+            interVals.add(new Interval(interval[0],interval[1]));
+        }
+        interVals.add(new Interval(newInterval[0],newInterval[1]));
+        Collections.sort(interVals,(a, b)->Integer.compare(a.start,b.start));
+        
+        List<Interval> mergedIntervals=new ArrayList<>();
+        Interval interval=interVals.get(0);
+        int start=interval.start,end=interval.end;
+        
+        for(int i=1;i<interVals.size();i++){
+            interval=interVals.get(i);
+            if(interval.start<=end){
+                end=Math.max(interval.end,end);
+            }else{
+                mergedIntervals.add(new Interval(start,end));
+                start=interval.start;
+                end=interval.end;
             }
         }
-
-        if(!isInserted)
-            list.add(Arrays.asList(start, end));
-
-        int[][] ans = new int[list.size()][2];
-
-        for(int i=0;i<list.size();i++){
-            ans[i][0] = list.get(i).get(0);
-            ans[i][1] = list.get(i).get(1);
+        mergedIntervals.add(new Interval(start,end));
+        int[][] result=new int[mergedIntervals.size()][2];
+        for(int i=0;i<mergedIntervals.size();i++){
+            result[i][0]=mergedIntervals.get(i).start;
+            result[i][1]=mergedIntervals.get(i).end;
         }
-        return ans;
+        return result;
+         
+        
+
+    }
+    class Interval{
+        int start,end;
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
     }
 }
