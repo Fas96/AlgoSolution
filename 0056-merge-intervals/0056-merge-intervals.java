0bdcb36c4e8532ec9257ebccc0c9decb1f0e5d86
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-27 18:03:17 +0900

    Time: 9 ms (72.97%), Space: 46.2 MB (87.36%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+     List<InterVal> interVals=new ArrayList<>();
+        for(int[] interval:intervals){
+            interVals.add(new InterVal(interval[0],interval[1]));
+        }
+        Collections.sort(interVals,(a, b)->Integer.compare(a.start,b.start));
+        
+        List<InterVal> mergedIntervals=new ArrayList<>();
+        
+        InterVal interval=interVals.get(0);
+        int start=interval.start,end=interval.end;
+        
+        for(int i=1;i<interVals.size();i++){
+            interval=interVals.get(i);
+            if(interval.start<=end){
+                end=Math.max(interval.end,end);
+            }else{
+                mergedIntervals.add(new InterVal(start,end));
+                start=interval.start;
+                end=interval.end;
+            }
+        }
+        mergedIntervals.add(new InterVal(start,end));
+        
+        int[][] result=new int[mergedIntervals.size()][2];
+        
+        for(int i=0;i<mergedIntervals.size();i++){
+            result[i][0]=mergedIntervals.get(i).start;
+            result[i][1]=mergedIntervals.get(i).end;
+        }
+        return result;
+    }
+    class InterVal{
+        int start,end;
+        public InterVal(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
