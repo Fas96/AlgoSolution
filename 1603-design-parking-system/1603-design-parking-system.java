c3e8c1d65f1cc849e0e0033662fda43741d3dbc6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-05-29 17:53:39 +0900

    Time: 11 ms (7.13%), Space: 44.6 MB (6.49%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -0,0 +1,26 @@
+class ParkingSystem {
+
+   Map<Integer,Integer> parkingMap;
+
+        public ParkingSystem(int big, int medium, int small) {
+            parkingMap = new java.util.HashMap<>();
+            
+            parkingMap.put(1,big);
+            parkingMap.put(2,medium);
+            parkingMap.put(3,small);
+        }
+
+        public boolean addCar(int carType) {
+            if(parkingMap.get(carType) > 0){
+                parkingMap.put(carType,parkingMap.get(carType) - 1);
+                return true;
+            }
+            return false;
+        }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
