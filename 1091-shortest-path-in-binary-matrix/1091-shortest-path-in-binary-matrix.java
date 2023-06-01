dc9c0f5b3d966f041fbd4f85cf81af82b51d3acb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-01 09:14:58 +0900

    Time: 12 ms (98.04%), Space: 45.5 MB (6.05%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,32 +1,26 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-      int n=grid.length;
-        if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
-        if(n==1) return 1;
-        int[][] dirs={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-        int[][] visited=new int[n][n];
-        visited[0][0]=1;
-        int res=1;
-        while(true){
+        int n = grid.length;
+        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
+        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+        int res = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
             res++;
-            boolean flag=false;
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(visited[i][j]==res-1){
-                        for(int[] dir:dirs){
-                            int x=i+dir[0];
-                            int y=j+dir[1];
-                            if(x>=0&&x<n&&y>=0&&y<n&&grid[x][y]==0&&visited[x][y]==0){
-                                if(x==n-1&&y==n-1) return res;
-                                visited[x][y]=res;
-                                flag=true;
-                            }
-                        }
-                    }
+            while(size-- > 0){
+                int[] cur = q.poll();
+                int x = cur[0], y = cur[1];
+                if(x == n - 1 && y == n - 1) return res;
+                for(int[] dir : dirs){
+                    int nx = x + dir[0], ny = y + dir[1];
+                    if(nx < 0 || nx >= n || ny < 0 || ny >= n || grid[nx][ny] == 1) continue;
+                    grid[nx][ny] = 1;
+                    q.add(new int[]{nx,ny});
                 }
             }
-            if(!flag) return -1;
         }
-          
+        return -1;
     }
 }
