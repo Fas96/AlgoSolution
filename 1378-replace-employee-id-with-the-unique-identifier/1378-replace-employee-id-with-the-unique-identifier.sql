7d2423c8396ca536ad1cd9381c65c245e6f2bf7c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-03 19:50:05 +0900

    Time: 2706 ms (18.56%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 SELECT uid.unique_id, e.name 
-FROM Employees e left join EmployeeUNI uid ON  e.id = uid.id 
+FROM Employees e LEFT JOIN EmployeeUNI uid ON  e.id = uid.id 
