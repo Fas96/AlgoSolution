156220c023d46b51d1eee580761b1ec1d717c8d6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-12 17:11:27 +0900

    Time: 6 ms (33.52%), Space: 41.4 MB (6.21%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-         List<String> ans=new LinkedList<>();
+        List<String> ans=new LinkedList<>();
         int n=nums.length;
-        int i=0;
-        while (i<n){
-            int j=i+1;
-            while (j<n && (nums[j] - nums[j-1]==1))j++;
-            ans.add((i == j-1)?nums[i]+"" : nums[i]+"->"+nums[j-1]);  
-            i=j;
+        int start=0;
+        while (start<n){
+            int end=start+1;
+            while (end<n && (nums[end] - nums[end-1]==1))end++;
+            ans.add((start == end-1)?nums[start]+"" : nums[start]+"->"+nums[end-1]);  
+            start=end;
         } 
         return ans;
     }
