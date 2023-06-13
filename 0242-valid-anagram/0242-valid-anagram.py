3143760a97503832512e974907bf7ec0c9f203c7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-14 00:08:55 +0900

    Time: 68 ms (48.48%), Space: 17.5 MB (9.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,6 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        hs,ht ={},{}
-        for ch in s:
-            if ch in hs:
-                hs[ch]+=1
-            else:
-                hs[ch] = 1
-                
-        for ch in t:
-            if ch in ht:
-                ht[ch] += 1
-            else:
-                ht[ch] = 1
-        return ht == hs
+    def isAnagram(self, s: str, t: str) -> bool: 
+        return sorted(t) == sorted(s)
                 
         
         
