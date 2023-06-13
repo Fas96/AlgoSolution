2d76dc7ded0e34dc6389c163c2d8fb00075ca78e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-14 00:18:14 +0900

    Time: 46 ms (95.22%), Space: 16.7 MB (78.80%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,11 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool: 
-        return sorted(t) == sorted(s)
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        for letter in letters:
+            if s.count(letter) != t.count(letter):
+                return False
+        return True
                 
         
         
