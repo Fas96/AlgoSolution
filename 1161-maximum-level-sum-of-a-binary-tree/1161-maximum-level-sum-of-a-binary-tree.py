1db7429169f2782acf5cea21c5161ad621041720
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-15 17:50:47 +0900

    Time: 320 ms (38.36%), Space: 21.5 MB (9.52%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -13,23 +13,26 @@ class Solution:
             if not Node:
                 return
             queueUse = deque([Node])
-
             while queueUse:
                 tempLevelValues = []
+                
                 for n in range(len(queueUse)):
-                    curLement = queueUse.popleft()
+                    curLement = queueUse.popleft() 
                     if curLement:
                         tempLevelValues.append(curLement.val)
                         queueUse.append(curLement.left)
                         queueUse.append(curLement.right)
+                        
                 if tempLevelValues:
                     levelTraversed.append(tempLevelValues)
         
         bfs(root)
         levelSum = float("-inf")
         heightOfLevel = 0 
+        
         for idx in range(len(levelTraversed)):
-            if sum(levelTraversed[idx])  > levelSum:
-                levelSum = sum(levelTraversed[idx])
+            temp=sum(levelTraversed[idx]) 
+            if temp>levelSum:
+                levelSum = max(levelSum,temp)
                 heightOfLevel = idx + 1 
         return heightOfLevel 
