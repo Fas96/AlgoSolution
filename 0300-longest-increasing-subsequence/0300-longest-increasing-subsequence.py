f2509e9ef3f517e3eae3cb1d665946971aa02691
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-16 17:54:21 +0900

    Time: 3516 ms (24.05%), Space: 16.7 MB (27.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,11 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int: 
-        dp=[]
-        for n in nums:
-            idx=bisect.bisect_left(dp,n)
-            if idx < len(dp):
-                dp[idx]=n
-            else:
-                dp.append(n)
-        return len(dp)
+        LIS=[1]*(len(nums))
+        for idx in range(len(nums)-1,-1,-1):
+            for jdx in range(idx+1,len(nums)):
+                if nums[idx]<nums[jdx]:
+                    LIS[idx]=max(LIS[idx],1+LIS[jdx])
+        return max(LIS)
+#         dp=[]
+#         for n in nums:
+#             idx=bisect.bisect_left(dp,n)
+#             if idx < len(dp):
+#                 dp[idx]=n
+#             else:
+#                 dp.append(n)
+#         return len(dp)
         
