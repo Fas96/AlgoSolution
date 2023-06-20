fca590b76e323550b841912766046acde4b42113
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-20 09:30:27 +0900

    Time: 1577 ms (93.25%), Space: 34.4 MB (88.55%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
@@ -1,15 +1,10 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-#         ans=[-1]*(len(nums))
+        ans=[-1]*(len(nums))
+        prefix=list(accumulate([0] + nums))
         
-#         if len(ans) > k:
-#             for i in range(k,len(nums)-k): 
-#                 ans[i]= math.floor(sum(nums[i:i+k+1]+nums[i-k:i])/(len(nums[i:i+k+1]+nums[i-k:i]))) 
-        prefix = list(accumulate([0] + nums))
-        def k_radius(i):
-            if i - k < 0 or i + k >= len(nums):
-                return -1
-            return (prefix[i + k + 1] - prefix[i - k]) // (2 * k + 1)
-        
-        return [k_radius(i) for i in range(len(nums))] 
+        if len(ans) > k:
+            for i in range(k,len(nums)-k): 
+                ans[i]= (prefix[i + k + 1] - prefix[i - k]) // (2 * k + 1) 
+        return  ans
         
