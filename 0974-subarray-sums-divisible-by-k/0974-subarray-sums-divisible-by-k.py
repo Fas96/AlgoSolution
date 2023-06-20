048f8d7cf022e4770931aa261361a1d5a704c499
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-20 21:53:16 +0900

    Time: 297 ms (86.74%), Space: 21 MB (13.26%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,15 +1,16 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        return sum(y*(y-1)//2 for y in Counter(x%k for x in accumulate([0] + nums)).values())
         
-        pre=list(accumulate([0]+nums))
-        cnt=[0 for _ in range(k)]
-        ans=0
-        for x in pre:
-            ans+=cnt[x%k]
-            cnt[x%k]+=1
-        return ans
+#         pre=list(accumulate([0]+nums))
+#         cnt,ans=[0]*k,0 
+        
+#         for x in pre:
+#             ans+=cnt[x%k]
+#             cnt[x%k]+=1
+        # return ans
+    
             
-        
         
 #         ps,hm,ans=list(accumulate([0]+nums)),{},0 
 #         print([0 for _ in range(k)])
