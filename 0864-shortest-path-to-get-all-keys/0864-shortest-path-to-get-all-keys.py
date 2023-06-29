9a9db7355eabbbf3b4fc1f8d4169ae8745f87d3f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-06-29 09:43:41 +0900

    Time: 651 ms (15.65%), Space: 23.4 MB (28.11%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.py
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.py
@@ -28,7 +28,7 @@ class Solution:
                 for x, y in directions:
                     new_keys = set(keys) # create set from tuple so we can easily compare if we have key for lock we may encounter
                     row, col = i + x, j + y # new coordinates
-                    if row >= 0 and col >= 0 and row < ROWS and col < COLS and grid[row][col] != '#':  
+                    if row >= 0 and col >= 0 and row < ROWS and col < COLS and grid[row][col] != '#': # if the row and column is in range of grid and not a wall.
                         if grid[row][col] in 'ABCDEF' and grid[row][col].lower() not in new_keys:
                             continue # then we do not have the key for this lock and cannot pass
                         if grid[row][col].islower() and grid[row][col] != '.':
@@ -38,4 +38,4 @@ class Solution:
                         else:
                             queue.append((row, col, tuple(new_keys), moves+1)) # havent obtained a new key but is a valid move
 
-        return -1 # we have exhausted all options
+        return -1 
