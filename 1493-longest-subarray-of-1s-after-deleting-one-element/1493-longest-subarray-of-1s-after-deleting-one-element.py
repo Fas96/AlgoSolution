316743a37a30662702904571ff721ab1a1458401
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-05 09:17:43 +0900

    Time: 440 ms (10.10%), Space: 19 MB (40.09%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,15 +1,14 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
+        
         n = len(nums)
+        defauldict = defaultdict(int)
         l = 0
         ans = 0
-        count = 0
         for r in range(n):
-            if nums[r] == 0:
-                count += 1
-            while count > 1:
-                if nums[l] == 0:
-                    count -= 1
+            defauldict[nums[r]] += 1
+            while defauldict[0] > 1:
+                defauldict[nums[l]] -= 1
                 l += 1
             ans = max(ans, r - l)
         return ans
