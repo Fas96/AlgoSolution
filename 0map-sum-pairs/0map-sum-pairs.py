5e9a3d353a0aa45e649c5fb3b7c81751d6975756
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-08 22:03:07 +0900

    Time: 51 ms, Memory: 16.3 MB - LeetHub

---
+++ 0map-sum-pairs/0map-sum-pairs.py
@@ -0,0 +1,19 @@
+class MapSum:
+
+    def __init__(self):
+        self.map={}
+        
+
+    def insert(self, key: str, val: int) -> None:
+        self.map[key]=val
+        
+
+    def sum(self, prefix: str) -> int:
+        return sum(val for key, val in self.map.items() if key.startswith(prefix))
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
