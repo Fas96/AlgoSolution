1de19a27ece36e3d636773a4fb217192a07b334a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-15 11:20:59 +0900

    Time: 886 ms (88.15%), Space: 197.7 MB (28.15%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,27 +1,15 @@
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
-        events.sort(key=lambda x: x[0])
-
-        def dfs(events, k, idx, memo):
-            if k == 1:
-                mx = 0
-                while idx < len(events):
-                    mx = max(events[idx][2], mx)
-                    idx += 1
-                return mx
-            if memo[k][idx] != -1:
-                return memo[k][idx]
-            res = 0
-            for i in range(idx, len(events)):
-                cur = events[i][2]
-                for j in range(i + 1, len(events)):
-                    if events[j][0] > events[i][1]:
-                        res = max(res, cur + dfs(events, k - 1, j,memo))
-                res = max(res, cur)
-            memo[k][idx]=res
-            return  memo[k][idx]
-
-        memo = [[-1] * len(events) for _ in range(k + 1)]
-
-        return dfs(events, k, 0, memo)
+        events.sort()
+        n = len(events)
+        @lru_cache(None)
+        def dp(i, k):
+            if i == n or k == 0:
+                return 0 
+            ans = dp(i + 1, k)
+             
+            next_i = bisect_left(events, [events[i][1] + 1])
+            ans = max(ans, events[i][2] + dp(next_i, k - 1))
+            return ans
+        return dp(0, k)
         
