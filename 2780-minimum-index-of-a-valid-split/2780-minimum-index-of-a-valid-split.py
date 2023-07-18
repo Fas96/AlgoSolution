271c95dbc56c5a5319d91097d03081f0d80be6c3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-18 21:38:57 +0900

    Time: 687 ms (94.38%), Space: 32.1 MB (50.21%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -1,21 +1,20 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        N = len(nums)
-        c = collections.Counter(nums)
-        mx = 0
-        mxi = 0
-        for k in c.keys():
-            if c[k] > mx:
-                mx = c[k]
-                mxi = k
+        m = len(nums)
+        freq = collections.Counter(nums)
+        dominant = None
+        for k, count in freq.items():
+            if count * 2 > m:
+                dominant = k
+                break
+        if dominant is None:
+            return -1
 
-        prefix = [0] * (N + 1)
-        for i in range(N):
-            prefix[i + 1] += prefix[i]
-            if nums[i] == mxi:
-                prefix[i + 1] += 1
-
-        for i in range(1, N):
-            if prefix[i] * 2 > i and (prefix[N] - prefix[i]) * 2 > N - i:
-                return i - 1
+        left, right = 0, freq[dominant]
+        for i, a in enumerate(nums):
+            if a == dominant:
+                left += 1
+                right -= 1
+            if left * 2 > i + 1 and right * 2 > m - i - 1:
+                return i
         return -1
