697d9d783d56d5a2aa967fd2c29fe0cbbd98b1d7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-28 10:10:19 +0900

    Time: 41 ms (96.36%), Space: 16.6 MB (19.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -4,17 +4,19 @@ class Solution:
         tb = defaultdict(int)
 
         def op(nums: List, left: int, right: int):
+            if (left, right) not in tb:
+                # last one remaining
+                if left == right:
+                    return nums[left]
+               
+                p_left = nums[left] - op(nums, left + 1, right)
+                p_right = nums[right] - op(nums, left, right - 1)
 
-            if left == right:
-                return nums[left]
-
-            if (left, right) in tb:
+                tb[(left, right)] = max(p_left, p_right)
+            else:
                 return tb[(left, right)]
+                
             
-            p_left = nums[left] - op(nums, left + 1, right)
-            p_right = nums[right] - op(nums, left, right - 1)
-
-            tb[(left, right)] = max(p_left, p_right)
             return tb[(left, right)] 
         
         return op(nums, left=0, right=len(nums) - 1) >= 0
