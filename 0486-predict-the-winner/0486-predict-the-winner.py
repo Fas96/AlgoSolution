cf0660da28372258501fc415415d1bdf6f7119c0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-28 10:06:31 +0900

    Time: 38 ms (97.94%), Space: 16.6 MB (19.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,19 +1,20 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
          
-        self.tb = defaultdict(int)
+        tb = defaultdict(int)
 
         def op(nums: List, left: int, right: int):
 
             if left == right:
                 return nums[left]
 
-            if (left, right) in self.tb:
-                return self.tb[(left, right)]
-            choose_left = nums[left] - op(nums, left + 1, right)
-            choose_right = nums[right] - op(nums, left, right - 1)
+            if (left, right) in tb:
+                return tb[(left, right)]
+            
+            p_left = nums[left] - op(nums, left + 1, right)
+            p_right = nums[right] - op(nums, left, right - 1)
 
-            self.tb[(left, right)] = max(choose_left, choose_right)
-            return self.tb[(left, right)] 
+            tb[(left, right)] = max(p_left, p_right)
+            return tb[(left, right)] 
         
         return op(nums, left=0, right=len(nums) - 1) >= 0
