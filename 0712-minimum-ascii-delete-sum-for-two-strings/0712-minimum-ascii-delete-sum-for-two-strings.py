528492f0e555fb2f7a7a5d9aa2397d3b4f303e51
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-07-31 17:02:52 +0900

    Time: 615 ms (76.25%), Space: 35.5 MB (28.86%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,15 +1,12 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        mp=collections.defaultdict(int)
         m, n = len(s1), len(s2)
-
-        @lru_cache(None)
-        def dp(i, j):
-            if i == m:
-                return sum(map(ord, s2[j:]))
-            if j == n:
-                return sum(map(ord, s1[i:]))
-            if s1[i] == s2[j]:
-                return dp(i + 1, j + 1)
-            return min(ord(s1[i]) + dp(i + 1, j), ord(s2[j]) + dp(i, j + 1))
-
-        return dp(0, 0)
+        for i in range(m):
+            for j in range(n):
+                if s1[i]==s2[j]:
+                    mp[i,j]=mp[i-1,j-1]+ord(s1[i])
+                else:
+                    mp[i,j]=max(mp[i-1,j],mp[i,j-1])
+        t_ascii=sum(map(ord,s1))+sum(map(ord,s2))
+        return t_ascii-2*mp[m-1,n-1]
