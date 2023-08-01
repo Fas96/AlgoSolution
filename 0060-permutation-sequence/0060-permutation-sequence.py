902ebaaabe3656c44aada0a1aedc64eb70a1cdaa
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-08-01 13:30:43 +0900

    Time: 1546 ms (17.12%), Space: 59.7 MB (6.55%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,6 +1,5 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        ans=permutations(range(1,n+1))
-        for i in range(k-1):
-            next(ans)
-        return ''.join(map(str,next(ans)))
+        ans=list(permutations(range(1,n+1)))
+      
+        return ''.join(map(str,ans[k-1]))
