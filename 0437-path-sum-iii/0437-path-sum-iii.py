4114bf28ee1fbc5a5d1b9e033cb782a945c48de5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-08-13 23:50:42 +0900

    Time: 2139 ms (5.00%), Space: 172.4 MB (5.55%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -8,6 +8,7 @@ class Solution:
     ans=0
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if not root: return 0
+        @cache
         def dfs(node,target):
             if not node: return 0
             if (node.val==target):self.ans+=1
