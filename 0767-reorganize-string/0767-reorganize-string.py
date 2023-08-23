310cbe30dfe603ff27d12d0ffdea50fe2f466694
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-08-23 15:05:38 +0900

    Time: 48 ms (45.66%), Space: 16.5 MB (9.58%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,8 +1,9 @@
 class Solution(object):
-    def reorganizeString(self, S):
-        a = sorted(sorted(S), key=S.count)
+    def reorganizeString(self, s):
+        a = sorted(sorted(s), key=s.count)
         h = len(a) / 2
-        a[1::2], a[::2] = a[:h], a[h:]
+        a[1::2], a[::2] = a[:int(h)], a[int(h):]
         return ''.join(a) * (a[-1:] != a[-2:-1])
+        
         
         
