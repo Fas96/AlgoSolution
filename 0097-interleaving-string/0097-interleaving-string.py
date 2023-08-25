ae616384b2b3b9c20f8ffe21c2c191fce2ac8a79
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-08-25 17:49:28 +0900

    Time: 58 ms (42.62%), Space: 19.6 MB (5.02%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,10 +1,8 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         def isInterleaving(X, Y, S, T):
-            if not X and not Y and not S:
-                return True 
-            if not S:
-                return False 
+            if not X and not Y and not S:  return True 
+            if not S:  return False 
             key = (X, Y, S) 
             if key not in T: 
                 x = (len(X) and S[0] == X[0]) and isInterleaving(X[1:], Y, S[1:], T) 
