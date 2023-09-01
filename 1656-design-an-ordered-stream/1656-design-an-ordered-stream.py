b9329aa231145c705555a8ee0a1835791132de98
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-01 22:36:49 +0900

    Time: 205 ms (43.42%), Space: 17.2 MB (15.35%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,13 +1,11 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.map={i:"" for i in range(n)}
-        self.ptr=0
+        self.map={i:"" for i in range(1,n+1)}
+        self.ptr=1
     def insert(self, idKey: int, value: str) -> List[str]:
         res=[]
-        self.map[idKey-1]=value
-         
-        
+        self.map[idKey]=value
         while (self.ptr in self.map.keys()) and len(self.map[self.ptr])>0:
             res.append(self.map[self.ptr])
             self.ptr+=1
