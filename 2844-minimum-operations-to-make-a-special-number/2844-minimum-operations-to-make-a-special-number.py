9aecac5cf574cce4c74f4e7b8541f253d436cbea
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-05 20:43:43 +0900

    Time: 200 ms (5.01%), Space: 19.9 MB (5.47%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.py
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def minimumOperations(self, num: str) -> int:
-        n = len(num)
-        
+        n = len(num) 
         @cache
-        def bactrack(index, current):
+        def pick(index, current):
             if index == n:
                 return 0 if current == 0 else float('inf')
-            return  min(bactrack(index + 1, (10 * current + int(num[index])) % 25), bactrack(index + 1, current) + 1) 
-        return bactrack(0, 0)
+            return  min(pick(index + 1, (10 * current + int(num[index])) % 25), pick(index + 1, current) + 1) 
+        return pick(0, 0)
         
