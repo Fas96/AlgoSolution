c804580b823f487e172e51b1634fa30d3b03b1dc
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-11 13:44:01 +0900

    Time: 69 ms (84.71%), Space: 16.3 MB (91.52%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -3,8 +3,7 @@ class Solution:
         mp=defaultdict(list)
         ans=[]
         for i, size in enumerate(groupSizes):
-            if size not in mp:
-                mp[size]=[]
+            if size not in mp:mp[size]=[]
             mp[size].append(i)
             if len(mp[size]) == size:
                 ans.append(mp[size])
