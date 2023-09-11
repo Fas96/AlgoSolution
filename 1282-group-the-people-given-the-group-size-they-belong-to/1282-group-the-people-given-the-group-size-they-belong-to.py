084c8b0ada14af74e6237970c857170114e2a0ff
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-11 13:41:38 +0900

    Time: 79 ms (45.31%), Space: 16.5 MB (31.58%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,22 +1,14 @@
 class Solution:
-    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        
-        def chunks(xs, n):
-            n = max(1, n)
-            return (xs[i:i+n] for i in range(0, len(xs), n))
-        
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]: 
         mp=defaultdict(list)
-        for i in range(len(groupSizes)):
-            if groupSizes[i] in mp:
-                mp[groupSizes[i]].append(i)
-            else:
-                mp[groupSizes[i]]=[i]
         ans=[]
-        for key,val in mp.items():
-            if len(val)>key:
-                for x in chunks(val,key): 
-                    ans.append(x)
-            else:
-                ans.append(val)
+        for i, size in enumerate(groupSizes):
+            if size not in mp:
+                mp[size]=[]
+            mp[size].append(i)
+            if len(mp[size]) == size:
+                ans.append(mp[size])
+                mp[size] = []
+      
         return ans
         
