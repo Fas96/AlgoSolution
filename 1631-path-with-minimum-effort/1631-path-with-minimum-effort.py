fd3ece51a23075a4feeef826711df1463a531f00
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-16 10:33:35 +0900

    Time: 568 ms (82.45%), Space: 18.8 MB (32.06%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -3,14 +3,18 @@ class Solution:
         m, n = len(heights), len(heights[0])
         pq = [(0, 0, 0)]
         visited = set()
+         
         while pq:
             effort, i, j = heapq.heappop(pq)
             if (i, j) in visited: continue
             visited.add((i, j))
             if i == m - 1 and j == n - 1: return effort
+          
             for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 ni, nj = i + di, j + dj
                 if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited:
                     newEffort = max(effort, abs(heights[i][j] - heights[ni][nj]))
+                    
                     heapq.heappush(pq, (newEffort, ni, nj))
+         
         return -1
