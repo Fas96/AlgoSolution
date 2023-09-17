75c9a7ff46e3113e71ddd90ca1a1855d0d3e9444
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-17 16:41:15 +0900

    Time: 1235 ms (51.44%), Space: 19.2 MB (39.69%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
@@ -3,14 +3,19 @@ class Solution:
         m, n = len(grid), len(grid[0])
         pq = [(-grid[0][0], 0, 0)]
         visited = set()
+        p=[]
+        
         while pq:
             val, i, j = heapq.heappop(pq)
             if (i, j) in visited: continue
             visited.add((i, j))
-            if i == m - 1 and j == n - 1: return -val
+            if i == m - 1 and j == n - 1:  
+                return -val 
             for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 ni, nj = i + di, j + dj 
                 if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited:
                     newval = max(val, -grid[ni][nj])
+                    
                     heapq.heappush(pq, (newval, ni, nj))
+  
         return -1
