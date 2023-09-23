b9f4e0efb49238d7aec46e8599239e2d000e0574
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-23 16:09:16 +0900

    Time: 1124 ms (26.62%), Space: 16.6 MB (93.85%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        
         words.sort(key=len)
-        dic = {}
-        
-        for i in words:
-            dic[ i ] = 1
-            
-            for j in range(len(i)):
-                print("--")
-                # creating words by deleting a letter
-                successor = i[:j] + i[j+1:]
-                print(i[:j] + i[j+1:],"==",i[:j] , i[j+1:])
-                if successor in dic:
-                    dic[ i ] = max (dic[i], 1 + dic[successor])
-        
-        res = max(dic.values())
-        return res
+        def isSubsequence(s ,t):
+            i, j = 0, 0
+            while i < len(s) and j < len(t):
+                if s[i] == t[j]:
+                    i += 1
+                j += 1
+            return i == len(s)
+        dp = [1] * len(words)
+        for i in range(len(words)):
+            for j in range(i):
+                if len(words[i]) - len(words[j]) == 1 and isSubsequence(words[j], words[i]):
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+                
