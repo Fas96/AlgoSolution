4bde27c957904d83b1d590b06189f97f61d7e231
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-09-25 15:17:21 +0900

    Time: 42 ms (54.89%), Space: 16.3 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,18 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        c = 0
-        for ch in s:
-            c ^= ord(ch)
-        for ch in t:
-            c ^= ord(ch)
-        return chr(c)
+    def findTheDifference(self, s: str, t: str) -> str: 
+        mt={}
+        for c in t:
+            if c in mt:
+                mt[c]+=1
+            else:
+                mt[c]=1
+        for c in s:
+            mt[c]-=1
         
+        ans=""
+        for k,v in mt.items():
+            if v>0:
+                ans+=k
+        
+            
+        return ans
