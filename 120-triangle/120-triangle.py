fa0a6dc9382e7e365284b590a8b121734bf1dc99
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-10-04 23:43:52 +0900

    Update 120-triangle.py

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,5 +3,4 @@ class Solution:
         for i in range(1, len(triangle)):
             for j in range(i + 1):
                 triangle[i][j] += min(triangle[i - 1][j - (j == i)], triangle[i - 1][j - (j > 0)])
-
         return min(triangle[-1])
