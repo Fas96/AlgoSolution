28fc3e1e52c604f760e962706b27976e3995c572
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-10-04 23:44:39 +0900

    Update 120-triangle.py
    Update 120-triangle.py

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # minimumTotal
         for i in range(1, len(triangle)):
             for j in range(i + 1):
                 triangle[i][j] += min(triangle[i - 1][j - (j == i)], triangle[i - 1][j - (j > 0)])
