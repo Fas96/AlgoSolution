8e1563592271d11f01d87db4159143dcdeebec28
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-10-20 23:32:00 +0900

    Time: 35 ms (78.55%), Space: 16.3 MB (54.11%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,18 +1,16 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        fstk=[]
-        sstk=[]
-        for x in s:
-            if fstk and x=='#':
-                fstk.pop()
-            elif x!='#':
-                fstk.append(x)
-        for x in t:
-            if sstk and x=='#':
-                sstk.pop()
-            elif x!='#':
-                sstk.append(x)
-        return fstk==sstk
-                
-                
-        
+        i, j = len(s)-1, len(t)-1
+        ss = tt = 0
+        while 0 <= i or 0 <= j: 
+            while 0 <= i and (s[i] == "#" or ss): 
+                if s[i] == "#": ss += 1
+                else: ss -= 1
+                i -= 1
+            while 0 <= j and (t[j] == "#" or tt): 
+                if t[j] == "#": tt += 1
+                else: tt -= 1
+                j -= 1
+            if i < 0 and 0 <= j or 0 <= i and j < 0 or 0 <= i and 0 <= j and s[i] != t[j]: return False 
+            i, j = i-1, j-1
+        return True 
