eaa22a96e57edf0f6c7495f6357b680ea47a809b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-10-21 00:22:36 +0900

    Time: 163 ms (16.86%), Space: 17.5 MB (86.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # nums.sort()
-        # XOR 
-        return sum([idx for idx in range(len(nums) + 1)])-sum(nums)
-        
+        nums.sort()
+        l,r=0,len(nums)-1
+        for i in range(max(nums)+1):
+            i=bisect_left(nums,i)
+            print(i)
+            if i!=nums[i]:
+                return i
+        return nums[-1]+1
+            
