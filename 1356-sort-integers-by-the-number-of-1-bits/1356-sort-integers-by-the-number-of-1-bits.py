f104882b8df6a77a465398adf42806d3cdc7c368
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-10-31 00:35:10 +0900

    Time: 77 ms (29.96%), Space: 16.5 MB (47.11%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -3,8 +3,8 @@ class Solution:
         n=[]
         for i in arr:
              n.append([i,(bin(i)[2:]).count('1')])
-        print(n)
+     
         n=sorted(n, key=lambda x: (x[1], x[0]))
-        print(n)
+        
         return [x[0] for x in n]
         
