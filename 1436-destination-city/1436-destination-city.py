2efd218d99dad61f1c0601d0fb2e8e7625fd32d6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2023-12-16 05:06:44 +0900

    Time: 59 ms (74.48%), Space: 16.6 MB (13.29%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,25 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        df={}
+        st=[]
+        for p in paths:
+            if p[0] not in st:
+                st.append(p[0])
+            if p[1] not in st:
+                st.append(p[1])
+            if p[0] not in df:
+                df[p[0]]=[p[1]]
+            else:
+                df[p[0]].append(p[1])
+        
+    
+        print(st,df)
+        dfl=[k for k,v in df.items()] 
+        print(dfl)
+        for val in st:
+            if val not in dfl:
+                return val
+        return ''
+                
+                
+        
