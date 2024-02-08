c5766ede4a5ba7f76f17c2918d15a85141bffc70
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-02-08 23:53:49 +0900

    Time: 1171 ms (80.90%), Space: 16.5 MB (92.57%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,18 +1,19 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        c={}
-        def go(n):
-            if n==0: return 0
-            if n<0:return float("inf")
-            if n in c: return c[n]
-            mini = n 
-            i = 1
+        def backtrack(i, target, currLen, minLen):
+            # found one solution
+            if target == 0:
+                return min(minLen, currLen)
             
-            while i*i<=n:                                                 
-                mini = min(mini, go(n-(i*i)))
-                i+=1
-            c[n]=mini+1
-            return mini+1
+            # return minLen if can't find a solution
+            if i < 1 or currLen >= minLen:
+                return minLen
 
-        return go(n)
-        
+            # try to find possible max number
+            if i**2 <= target:
+                minLen = backtrack(i, target - i**2, currLen + 1, minLen)
+      
+            minLen = backtrack(i-1, target, currLen, minLen)
+            return minLen
+
+        return backtrack(int(math.sqrt(n)), n, 0, float('inf'))
