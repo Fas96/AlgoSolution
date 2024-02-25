d3df2785312740727e7202de2898793b85a3874b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-02-25 17:18:38 +0900

    Time: 606 ms (6.40%), Space: 22.9 MB (13.40%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        ans=[0,1]
+        for i in range(1,len(s)):
+            ans.append(ans[-1])  
+            BEG= max(0,i- maxJump)
+            END= max(0,i- minJump+1) 
+            if  (ans[END]-ans[BEG]>0) and (s[i] == '0') :
+                ans[-1]+=1
+        print(ans)
+        return ans[-1]>ans[-2]
+  
