255798c1d4e6e883ef06cc5c8e3d72a144458972
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-02-25 16:30:49 +0900

    Time: 255 ms (12.90%), Space: 19.5 MB (60.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,32 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        L,R,N=0,0,len(nums)
+        ans=0
+        curS=0 
+        ac=accumulate(nums)
+       
+        mp={0:1}
+        for x in ac:
+            if x-k in mp:
+                ans+=mp[x-k]
+            mp[x]=mp.get(x,0)+1
+            
         
-        ps = 0
-        count = 0
-        sum_freq = {0: 1} 
-        for num in nums:
-            ps += num
-            if ps - k in sum_freq:
-                count += sum_freq[ps - k]
-            sum_freq[ps] = sum_freq.get(ps, 0) + 1
-        return count
-#         c={}
-#         @cache
-#         def go(idx, cs):
-#             if idx == len(nums):
-#                 return 0
-#             count = 0
-#             if (idx,cs) in c:
-#                 return c[(idx,cs)]
-#             if cs + nums[idx] == k:
-#                 count += 1
-#             count += go(idx + 1, cs + nums[idx])
-#             c[(idx,cs)]=count
-#             return count
-
-#         ttc = 0
-#         for i in range(len(nums)):
-#             ttc += go(i, 0)
-
-#         return ttc
+        return ans
+                
         
