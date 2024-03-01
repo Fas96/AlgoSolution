2828259db8aaca8a7ea417d5be96367812f7d204
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-01 17:19:30 +0900

    Time: 35 ms (76.40%), Space: 16.7 MB (34.59%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -2,14 +2,17 @@ class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
         sbit=['0']*len(s)
         N=len(s)
-        leftMost=0 
+        leftMost=0
+        cnt=0;
         
         for i,c in enumerate(s):
             if c=='1':
-                if '1' not in sbit: sbit[N-1]='1'
+                if cnt==0: sbit[N-1]='1'
                 else: 
                     sbit[leftMost]='1'
-                    leftMost+=1 
+                    leftMost+=1
+                cnt+=1
+        
         
         print(sbit)
         
