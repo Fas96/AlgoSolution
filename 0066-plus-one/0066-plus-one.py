542576f0b67ae315a175a2f60502ea4d2e93ef18
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-02 15:23:51 +0900

    Time: 40 ms (34.00%), Space: 16.4 MB (89.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         N=len(digits)
-        s= ''.join(str(x) for x in digits)
-        ans=str(int(s)+1)
-        return [int(x) for x in ans]
+        pos=10
+        NUM=0
+        for x in digits:
+            NUM = (NUM*pos)+ (x) 
+        NUM+=1
+        return [int(d) for d in str(NUM)]
+        
