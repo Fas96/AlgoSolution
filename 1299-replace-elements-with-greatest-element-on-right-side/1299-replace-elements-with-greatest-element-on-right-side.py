81dd04da728df556b542fad299571f25ae154bf0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-04 01:55:35 +0900

    Time: 520 ms (47.26%), Space: 18.7 MB (36.12%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -4,7 +4,6 @@ class Solution:
         ans=[-1]*N  
         mx=-1
         for i in range(N-1,-1,-1):
-            mx=max(mx, ans[i])
             ans[i], mx = mx, max(mx, arr[i])
         ans[-1]=-1
         return ans
