0b530cd04a97449ae253435cc9360cec5e404fd3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-08 23:14:22 +0900

    Time: 115 ms (88.12%), Space: 22.2 MB (39.83%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,20 +1,20 @@
 class MyHashSet:
 
     def __init__(self):
-        self.map={}
+        self.mp={}
         
 
     def add(self, key: int) -> None:
-        self.map[key]=key
+        self.mp[key]=[]
         
 
     def remove(self, key: int) -> None:
-        if key in self.map:
-            del self.map[key]
+        if key in self.mp:
+            del self.mp[key]
         
 
     def contains(self, key: int) -> bool:
-        return key in self.map
+        return key in self.mp
         
 
 
