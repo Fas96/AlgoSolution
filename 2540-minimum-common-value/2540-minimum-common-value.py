c90ff0a3280da6368928887562cd33ad85ad975d
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-09 11:44:28 +0900

    Time: 360 ms (34.35%), Space: 41.7 MB (28.27%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,6 +1,8 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        ll=list(set(nums1) & set(nums2))
-        if len(ll)==0: return -1
-        return min(ll)
+        ll=Counter(nums1)
+        for x in nums2:
+            if x in ll:
+                return x
+        return  -1
         
