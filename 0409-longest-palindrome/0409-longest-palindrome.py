51fb852c1498aac079e62866c0b1a50723dbcfa3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-09 15:04:35 +0900

    Time: 45 ms (9.09%), Space: 16.6 MB (52.20%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -5,12 +5,8 @@ class Solution:
         odd=[]
         t=False
         for k,v in f.items():
-            if v%2==0:
-                ans+=v
-            else:
-                t=True
-                ans+=(v-1)
+            ans= ans+v if v%2==0 else ans+(v-1)
+            if v%2==1: t=True
                 
- 
         return ans+1 if t else ans
             
