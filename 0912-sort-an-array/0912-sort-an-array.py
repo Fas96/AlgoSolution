562977a9c9c5058f7b0ab4dd7265240822e6f116
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-10 02:31:10 +0900

    Time: 585 ms (89.34%), Space: 30.4 MB (16.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,11 +1,10 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        freq_dict = Counter(nums)
-                
-        min_num, max_num = min(nums),max(nums)
+        fq = Counter(nums)     
+        mnNum, mxNum = min(nums),max(nums)
         ans = []
-        for num in range(min_num, max_num + 1):
-            if num in freq_dict:
-                ans.extend([num]*freq_dict[num])
+        for num in range(mnNum, mxNum + 1):
+            if num in fq:
+                ans.extend([num]*fq[num])
         return ans
         
