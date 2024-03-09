c1ef4284c85c260b164c7ce98695fa1ef0b63244
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-10 02:29:54 +0900

    Time: 555 ms (90.93%), Space: 30.4 MB (16.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,28 +1,11 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def mergesort(arr):
-            if len(arr)>1:
-                mid=len(arr)//2
-                L,R=arr[:mid],arr[mid:]
-                mergesort(L)
-                mergesort(R)
-                i=j=k=0
-                while i<len(L) and j<len(R):
-                    if L[i]<=R[j]:
-                        arr[k]=L[i]
-                        i+=1
-                    else:
-                        arr[k]=R[j]
-                        j+=1
-                    k+=1
-                while i<len(L):
-                    arr[k]=L[i]
-                    i+=1
-                    k+=1
-                while j<len(R):
-                    arr[k]=R[j]
-                    j+=1
-                    k+=1
-        mergesort(nums)
-        return nums
+        freq_dict = Counter(nums)
+                
+        min_num, max_num = min(nums),max(nums)
+        ans = []
+        for num in range(min_num, max_num + 1):
+            if num in freq_dict:
+                ans.extend([num]*freq_dict[num])
+        return ans
         
