5613788042dbbca24db91c9da41202b05565e175
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-10 16:11:43 +0900

    Time: 149 ms (98.67%), Space: 25 MB (21.10%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,12 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         pf=list(accumulate(nums, operator.mul)) 
-        postfix=1
+       
+        suffix=1
         answer=[1]+pf
         for i in range(len(nums)-1,-1,-1):
-            answer[i] *= postfix
-            postfix *= nums[i]
-        
-            
-            
+            answer[i] *= suffix
+            suffix *= nums[i]
         return answer[:-1]
