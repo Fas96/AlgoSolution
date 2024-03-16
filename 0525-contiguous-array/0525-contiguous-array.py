f1473204ad0489e17172ce6ead79194637b7d7ac
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-16 16:21:35 +0900

    Time: 648 ms (13.07%), Space: 22.3 MB (33.26%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -5,10 +5,8 @@ class Solution:
         ans=0
         mp={0:-1}
         for i,x in enumerate(nums):
-            if x == 1:
-                ones+=1
-            if x == 0:
-                zeros+=1
+            if x == 1:ones+=1
+            if x == 0:zeros+=1
             d=zeros-ones
             if d in mp:
                 ans=max(ans,i-mp[d])
