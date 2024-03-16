386881555465638715f4c3cdd86087790bbcc735
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-16 17:18:32 +0900

    Time: 468 ms (54.92%), Space: 62.3 MB (12.69%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -3,10 +3,9 @@ class Solution:
         ans=0
         mp={0:-1}
         pf=list(accumulate(nums))
+         
         for i,x in enumerate(nums):
-            
             if pf[i]-k in mp:
                 ans=max(ans,i-mp[pf[i]-k])
-            mp.setdefault(pf[i],i)
-       
+            mp.setdefault(pf[i],i) 
         return ans
