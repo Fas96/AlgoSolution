db32e894131ef6d7e06b087ae17c62705bc40ea6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-22 01:48:11 +0900

    Time: 270 ms (33.30%), Space: 22.9 MB (43.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,68 +1,39 @@
-class UnionFind(object):
-    def __init__(self, grid):
-        row, col = len(grid), len(grid[0])
-        self.parent = ['w'] * (row * col)
-        self.count = 0
 
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == '1':
-                    self.parent[(col * r) + c] = -1
-    
-    def find(self, index):
-        if self.parent[index] == 'w':
-            return index
-        
-        if self.parent[index] > -1:
-            self.parent[index] = self.find(self.parent[index])
-            return self.parent[index]
-        
-        return index
-    
-    def union(self, vertexA, vertexB):
-        aIndex = self.find(vertexA)
-        bIndex = self.find(vertexB)
-
-        if aIndex == bIndex or self.isWater(aIndex, bIndex):
-            return
-        
-        if self.parent[aIndex] > self.parent[bIndex]:
-            self.parent[bIndex] += self.parent[aIndex]
-            self.parent[aIndex] = bIndex
-        else:
-            self.parent[aIndex] += self.parent[bIndex]
-            self.parent[bIndex] = aIndex
-    
-    def isWater(self, aIndex, bIndex):
-        return self.parent[aIndex] == 'w' or self.parent[bIndex] == 'w'
-		
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        class UnionFind:
+            def __init__(self, size):
+                self.root = [i for i in range(size)]
+                self.rank = [1] * size
+
+            def find(self, x):
+                while x != self.root[x]:
+                    x = self.root[x]
+                return x
+
+            def union(self, x, y):
+                rootX = self.find(x)
+                rootY = self.find(y)
+                if rootX == rootY: return False
+                self.root[rootX] = rootY
+                return True
+
+            def connected(self, x, y):
+                return self.find(x) == self.find(y)
+        DIR = [0, 1, 0, -1, 0]
+        m, n = len(grid), len(grid[0])
+        uf = UnionFind(m*n)
         
-        row, col = len(grid), len(grid[0])
-        uf = UnionFind(grid)
-
-        for r in range(row):
-            for c in range(col):
-                
-                if grid[r][c] is '1':
-                    parentIndex = (col * r) + c
-
-                    # right
-                    if c < col - 1:
-                        right = parentIndex + 1
-                        uf.union(parentIndex, right)
-                    
-                    # down
-                    if r < row - 1:
-                        down = (col * (r+1)) + c
-                        uf.union(parentIndex, down)
-        
-        islands = 0
-        for ele in uf.parent:
-            if ele is not 'w' and ele < 0:
-                islands +=1
-
-        return islands
+        component = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "0": continue
+                component += 1
+                curId = r * n + c
+                for i in range(4):
+                    nr, nc = r + DIR[i], c + DIR[i+1]
+                    if nr < 0 or nr == m or nc < 0 or nc == n or grid[nr][nc] == "0": continue
+                    neiId = nr * n + nc
+                    if uf.union(curId, neiId):
+                        component -= 1
+        return component
