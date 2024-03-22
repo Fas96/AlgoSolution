095f7f0e1e516d69b66d1fc6d379757ef0358f3b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-23 01:48:12 +0900

    Time: 90 ms, Memory: 18.3 MB - LeetHub

---
+++ graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,43 @@
+class UF:
+    def __init__(self, size):
+        self.root=[i for i in range(size)]
+        self.rank=[1]*(size)
+        self.count=size
+    def find(self,x):
+        if x==self.root[x]:    
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    def union(self,x,y):
+        rootX,rootY=self.find(x),self.find(y)
+        if rootX==rootY:
+            return
+        if self.rank[rootX]>self.rank[rootY]:
+            self.root[rootY]=self.root[rootX]
+        elif self.rank[rootX]<self.rank[rootY]:
+            self.root[rootX]=self.root[rootY]
+        else:
+            self.root[rootX]=self.root[rootY]
+            self.rank[rootY]+=1
+        self.count -= 1
+    def connected(self,x,y):
+        return self.find(x)==self.find(y)
+    def getCount(self):
+        return self.count
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges)==0 and n>1:
+            return False
+        uf=UF(n)
+        x=len(edges)
+     
+        N = n
+        for a, b in edges: 
+            parent_a, parent_b = uf.find(a), uf.find(b) 
+            if parent_a == parent_b:
+                return False 
+            N -= 1
+            uf.union(parent_a, parent_b) 
+        return N == 1
+    
+        
