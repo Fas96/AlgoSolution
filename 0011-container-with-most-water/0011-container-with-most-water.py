836c314a627a2b4e88b433d7a42d39e325e98e0c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-23 18:53:21 +0900

    Time: 537 ms (29.32%), Space: 29.7 MB (22.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,9 +6,10 @@ class Solution:
         while l<r:
             H=min(height[l],height[r])
             mxAr=max(mxAr,H*(r-l))
-            while height[l]<=H and  l<r :
-                l+=1 
-            while  height[r]<=H and  l<r :
-                r-=1 
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+            
         return mxAr
         
