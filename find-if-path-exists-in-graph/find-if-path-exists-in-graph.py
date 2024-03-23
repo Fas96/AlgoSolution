fc0f0b9ffa0bfee29632ff99c5299138797cc0c0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-24 04:07:14 +0900

    Time: 1431 ms, Memory: 108.6 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,31 +1,25 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        class UnionFind:
-            def __init__(self, size):
-                self.root = [i for i in range(size)]
-                self.rank = [1] * size
-
-            def find(self, x):
-                while x != self.root[x]:
-                    x = self.root[x]
-                return x
-
-            def union(self, x, y):
-                rootX = self.find(x)
-                rootY = self.find(y)
-                if rootX != rootY:
-                    if self.rank[rootX] > self.rank[rootY]:
-                        self.root[rootY] = rootX
-                    elif self.rank[rootX] < self.rank[rootY]:
-                        self.root[rootX] = rootY
-                    else:
-                        self.root[rootY] = rootX
-                        self.rank[rootX] += 1
-
-            def connected(self, x, y):
-                return self.find(x) == self.find(y)
-        uf=UnionFind(n)
-        for u,v in edges:
-            uf.union(u,v)
-        return uf.connected(source,destination)
-
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        
+        mp = {}
+        for a, b in edges:
+            if a not in mp:
+                mp[a]=[]
+            if b not in mp:
+                mp[b]=[]
+            mp[a].append(b)
+            mp[b].append(a)
+        
+        stack = [start]
+        seen = set()
+        
+        while stack: 
+            node = stack.pop() 
+            if node == end:
+                return True 
+            if node in seen:
+                continue
+            seen.add(node) 
+            for neighbor in mp[node]:
+                stack.append(neighbor) 
+        return False
