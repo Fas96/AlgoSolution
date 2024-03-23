a156e42ca9b6b97c27eca9f4e8c071a2172d976e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-23 17:13:36 +0900

    Time: 1873 ms (31.76%), Space: 21.4 MB (31.32%) - LeetHub

--- 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
@@ -1,13 +1,7 @@
 class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
         ans=[]
-        aa,ab=[],[]
-        for i in range(len(s)):
-            if s[i] == a[0] and s[i:i + len(a)] == a:
-                aa.append(i)
-            if s[i] == b[0] and s[i:i + len(b)] == b:
-                ab.append(i)
-        print(aa,ab)
+        aa,ab=[i for i in range(len(s)) if s.startswith(a, i)],[i for i in range(len(s)) if s.startswith(b, i)]
         for i in aa:
             for j in ab:
                 if abs(i-j)<=k:
