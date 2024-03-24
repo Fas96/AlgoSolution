d840cd13e27b529cb4319e17bbfe6c12abc2fa8f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-25 00:43:29 +0900

    Time: 37 ms (39.60%), Space: 16.6 MB (31.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n<=2:return n
-        a,b,c=1,2,0
+        first,second,ans=1,2,0
         for i in range(3,n+1):
-            c=a+b
-            a,b=b,c 
-        return c
+            ans=first+second
+            first,second=second,ans
+        return ans
