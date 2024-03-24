3886a58dffb8f9a90f5fa7a742d70bf91c911e89
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-25 00:42:27 +0900

    Time: 30 ms (83.51%), Space: 16.5 MB (31.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,9 @@
 class Solution:
-    def climbStairs(self, n: int) -> int: 
-         
-        @cache
-        def go(g):
-            if g==1 or g==2:
-                return g
-            return go(g-1)+go(g-2)
-        return go(n)
-        
+    def climbStairs(self, n: int) -> int:
+        if n<=2:return n
+        a,b,c=1,2,0
+        for i in range(3,n+1):
+            c=a+b
+            a=b
+            b=c
+        return c
