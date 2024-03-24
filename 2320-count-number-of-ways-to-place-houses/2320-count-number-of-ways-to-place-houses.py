99b1e569f23ec2504124b3c7eb6cd69f6fde8e67
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-03-25 00:50:07 +0900

    Time: 87 ms (74.43%), Space: 16.6 MB (56.25%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
@@ -1,7 +1,6 @@
 class Solution:
     def countHousePlacements(self, n: int) -> int:
         f,s=0,1
-        for i in range(n+1):
-            f,s=s,f+s
+        for i in range(n+1): f,s=s,f+s
         return (s*s)%((10**9)+7) 
         
