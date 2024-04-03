94c3f6748028c4aa1c3761182619a6d662372ed0
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-04-03 19:26:19 +0900

    Time: 51 ms (54.45%), Space: 16.6 MB (66.40%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,28 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q=deque()
-        fo=0
         r,c=len(grid),len(grid[0])
+        fo=0
         for i in range(r):
             for j in range(c):
                 if grid[i][j]==2:
                     q.append((i,j))
                 elif grid[i][j]==1:
                     fo+=1
+        mo=-1
         q.append((-1,-1))
-        me=-1
-        dirr = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         while q:
             cr,cc=q.popleft()
             if cr==-1:
-                me+=1
-                if q: q.append((-1,-1))
+                mo+=1
+                if q:q.append((-1,-1))
             else:
-                for ii,jj in dirr:
-                    ni,nj=cr+ii,cc+jj
+                assert (cr>-1 and cc>-1),"false"
+                for d1,d2 in [[1,0],[0,1],[-1,0],[0,-1]]:
+                    ni,nj=cr+d1,cc+d2
                     if r>ni>=0 and c>nj>=0 and grid[ni][nj]==1:
                         grid[ni][nj]=2
                         fo-=1
                         q.append((ni,nj))
-        return me if fo==0 else -1
+        return mo if fo==0 else -1
         
