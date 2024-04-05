f46a5fa98959eb0f02e466e19c6264af3ba236e4
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-04-05 13:30:56 +0900

    Time: 47 ms (7.68%), Space: 16.5 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,11 +1,12 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        stk=[] 
-        for x in s:
-            stk.append(x)
-            if len(stk)>1:
-                if (stk[-1]!=stk[-2]) and (stk[-1]==stk[-2].lower() or stk[-1].lower()==stk[-2]):
-                    stk.pop()
-                    stk.pop()
-        return ''.join(stk)
-        
+        n=len(s)
+        stk=[]
+        for i in range(len(s)):
+            if stk and abs(ord(stk[-1])-ord(s[i]))==32:
+                stk.pop()
+            else:
+                stk.append(s[i])
+        return "".join(stk)
+                
+            
