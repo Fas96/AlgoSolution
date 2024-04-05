4ab9a4188ba1cf846c806581b31a03d0b692830f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-04-05 16:41:00 +0900

    Time: 41 ms (28.60%), Space: 16.6 MB (39.00%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def numSquareSum(n):
-            squareSum = 0
-            while(n):
-                squareSum += (n % 10) * (n % 10)
+        def numSqSum(n):
+            sq = 0
+            while n:
+                sq += (n % 10) * (n % 10)
                 n = int(n / 10)
-            return squareSum
+            return sq
         sl=fs=n
         while True:
-            sl=numSquareSum(sl)
-            fs=numSquareSum(numSquareSum(fs))
+            sl=numSqSum(sl)
+            fs=numSqSum(numSqSum(fs))
             if sl!=fs:continue
             else: break
         return sl==1
