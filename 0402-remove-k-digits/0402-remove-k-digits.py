e344eab7e0ba91b5060e8db7f8fa25a1cfcb52f5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-04-11 13:59:17 +0900

    Time: 48 ms (90.16%), Space: 17.9 MB (92.29%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,16 +1,15 @@
-import sys
-sys.set_int_max_str_digits(0)
 class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
-        stk = []
-        for x in num:
-            while stk and k > 0 and stk[-1] > x:
+    def removeKdigits(self, num: str, k: int) -> str: 
+        if k==len(num):return "0"
+        rem=len(num)-k
+        stk=[]
+        for c in num:
+            while k and stk and stk[-1]>c:
                 stk.pop()
-                k -= 1
-            stk.append(x)
-
-        while stk and k > 0:
-            stk.pop()
-            k -= 1
-        return "0" if not stk else str(int("".join(stk)))
+                k-=1
+            stk.append(c)
+        return ''.join(stk[:rem]).lstrip('0') or '0'
+        
+            
+                    
         
