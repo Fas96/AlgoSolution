cbc2dcebc9c1f957bde63a09d701fe528f3147fc
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-04-19 14:43:56 +0900

    Time: 240 ms (67.01%), Space: 18.9 MB (61.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,19 @@
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        class UnionFind:
-            def __init__(self, size):
-                self.root = [i for i in range(size)]
-                self.rank = [1] * size
-
-            def find(self, x):
-                while x != self.root[x]:
-                    x = self.root[x]
-                return x
-
-            def union(self, x, y):
-                rootX = self.find(x)
-                rootY = self.find(y)
-                if rootX == rootY: return False
-                self.root[rootX] = rootY
-                return True
-
-            def connected(self, x, y):
-                return self.find(x) == self.find(y)
-        DIR = [0, 1, 0, -1, 0]
-        m, n = len(grid), len(grid[0])
-        uf = UnionFind(m*n)
+        nI=0
+        N=len(grid)
+        def dfs(grid,i,j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+                return
+            grid[i][j]='0'
+            for ni,nj in [[1,0],[-1,0],[0,1],[0,-1]]:
+                dfs(grid,i+ni,j+nj) 
+                
+        for i in range(N):
+            for j in range(len(grid[i])):
+                if grid[i][j]=='1':
+                    nI+=1
+                    dfs(grid,i,j)
         
-        T = 0
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == "0": continue
-                T += 1
-                curId = r * n + c
-                for i in range(4):
-                    nr, nc = r + DIR[i], c + DIR[i+1]
-                    if nr < 0 or nr == m or nc < 0 or nc == n or grid[nr][nc] == "0": continue
-                    neiId = nr * n + nc
-                    if uf.union(curId, neiId):
-                        T -= 1
-        return T
+        return nI
+        
