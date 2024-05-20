afbd58074737da0f543a412f1c4accb26d6ee948
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-05-20 15:38:55 +0900

    Time: 41 ms (78.60%), Space: 16.5 MB (84.04%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,21 +1,11 @@
 from functools import reduce
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def backtrack(arr, index, current):
-            if index == len(arr):
-                return [current[:]]
-            current.append(arr[index])
-            subsets_with_current = backtrack(arr, index + 1, current)
-            current.pop()
-            subsets_without_current = backtrack(arr, index + 1, current)
-            return subsets_with_current + subsets_without_current
- 
-        ans=0
-        for x in backtrack(nums, 0, []):
-            if len(x)<=0:
-                continue
-            ans+=(reduce(lambda x, y: x ^ y,x))
-        return ans
+        sumTotal = 0
+
+        for num in nums:
+            sumTotal |= num
+        return sumTotal << (len(nums) - 1) 
         
                 
         
