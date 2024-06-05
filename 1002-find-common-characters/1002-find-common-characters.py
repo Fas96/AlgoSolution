51e2e7349504b39b15db917d5f3caf352756fa8b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-06-05 15:18:04 +0900

    Time: 50 ms (53.96%), Space: 16.7 MB (67.21%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        ans=Counter(words[0])  
+        for word in words:
+            ans &= Counter(word)
+        
+        return list(ans.elements())
+        
