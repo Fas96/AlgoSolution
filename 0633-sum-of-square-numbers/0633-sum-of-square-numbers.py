8c03d2816835eab225c788a7a469950c6acc1f90
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-06-16 18:38:47 -0700

    Time: 186 ms (10.76%), Space: 25.8 MB (5.64%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,7 +1,5 @@
 class Solution:
-    def judgeSquareSum(self, c: int) -> bool:
-        # if c<=2:return True
-        
+    def judgeSquareSum(self, c: int) -> bool: 
         hashmap={}
         for i,x in enumerate([i*i for i in range(int(sqrt(c))+1)]):
             if x+x==c:return True
