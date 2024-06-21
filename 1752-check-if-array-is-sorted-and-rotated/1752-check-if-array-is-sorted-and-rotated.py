39e66e24a71a7553fb66fc98a5596c94f683f5fd
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-06-21 12:30:04 -0700

    Time: 25 ms (99.13%), Space: 16.5 MB (38.65%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,9 @@
+class Solution:
+    def check(self, B: List[int]) -> bool:
+        A=sorted(B)
+        n=len(A)
+        for i in range(n):
+            if A[i:]+A[:i]==B:
+                return True
+        return False
+        
