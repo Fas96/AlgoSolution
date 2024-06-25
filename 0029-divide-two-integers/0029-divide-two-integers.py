6896a33aa3d4225bd1f20e90b5cca817cc079789
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-06-25 07:20:40 -0700

    Time: 36 ms (64.84%), Space: 16.5 MB (90.19%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,5 @@
+from decimal import Decimal
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        return min(int(str(dividend/divisor).split('.')[0]),2**31-1) 
+        
