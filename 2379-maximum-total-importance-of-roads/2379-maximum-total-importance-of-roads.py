fb0412b1b649cc6c39229452febde7a8a8fa59ae
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-06-27 22:00:01 -0700

    Time: 1203 ms (79.92%), Space: 41.6 MB (73.77%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -2,8 +2,7 @@ class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         g=[0]*n
         for x in roads:
-            g[x[0]]+=1
-            g[x[1]]+=1
+            g[x[0]]+=1;g[x[1]]+=1;
         g.sort()
         ans=0
         for i in range(n):
