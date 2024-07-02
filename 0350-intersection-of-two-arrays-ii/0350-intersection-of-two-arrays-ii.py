72c01ebd6b96870db075a626bd67039fbb8aaa1e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-02 07:54:20 -0700

    Time: 38 ms (96.85%), Space: 16.7 MB (71.02%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,8 +1,5 @@
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        f1=Counter(nums1)
-        f2=Counter(nums2)
-        
-        return [k for k, v in (f1 & f2).items() for _ in range(v)]
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]: 
+        return [k for k, v in (Counter(nums1) & Counter(nums2)).items() for _ in range(v)]
 
         
