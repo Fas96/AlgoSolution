b37619e6f38d38460eb72a82532959dcabb4778a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-06 12:47:03 +0900

    Time: 26 ms (95.95%), Space: 16.6 MB (7.09%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.py
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -2,9 +2,6 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         cLen = 2 * (n - 1) 
         T = time % cLen
-         
-        if T < n: 
-            return T + 1
-        else: 
-            return n - (T - (n - 1))
+        
+        return T+1 if T < n else  n - (T - (n - 1))
         
