ad5cd46a562f6bc2f255d78b1bb0371bf01cd764
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-06 12:46:42 +0900

    Time: 47 ms (6.84%), Space: 16.5 MB (77.72%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.py
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -1,10 +1,10 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        cycle_length = 2 * (n - 1) 
-        effective_time = time % cycle_length
+        cLen = 2 * (n - 1) 
+        T = time % cLen
          
-        if effective_time < n: 
-            return effective_time + 1
+        if T < n: 
+            return T + 1
         else: 
-            return n - (effective_time - (n - 1))
+            return n - (T - (n - 1))
         
