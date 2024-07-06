fdf5c7f3136cc1402f90a9bfbaa852e84babf53c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-06 17:57:35 +0900

    Time: 29 ms (88.61%), Space: 16.4 MB (77.72%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.py
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -1,7 +1,13 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        cLen = 2 * (n - 1) 
-        T = time % cLen
-        
-        return T+1 if T < n else  n - (T - (n - 1))
-        
+        forward=True 
+        ans=1
+        for i in range(time,0,-1):
+            if forward:
+                ans+=1
+                if ans%n==0: forward=not forward
+            else:
+                ans-=1
+                if ans==1: forward=not forward
+       
+        return ans
