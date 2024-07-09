a8c0dd9fde59dc8d3e3282f856dcdaa42bc4c01d
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-09 23:47:50 +0900

    Time: 758 ms (37.46%), Space: 57.2 MB (45.36%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -0,0 +1,15 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        n=len(customers)
+        w=0
+        ans=0.0
+        for a,b in customers:
+            if a>w:
+                ans+=b
+                w=a+b
+            else:
+                w+=b
+                ans=ans+(w-a)
+
+        return ans/n
+        
