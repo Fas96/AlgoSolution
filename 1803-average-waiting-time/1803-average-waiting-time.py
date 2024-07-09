dd07e6f9c362db0edfb050a5a69b941242f384bb
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-09 23:56:13 +0900

    Time: 760 ms (35.74%), Space: 56.9 MB (85.22%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -1,11 +1,11 @@
 class Solution:
-    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+    def averageWaitingTime(self, c: List[List[int]]) -> float:
        
         w=ans=0
          
-        for a,b in customers:
+        for a,b in c:
             w =  max(w, a) + b
             ans += (w - a)
 
-        return ans/len(customers)
+        return ans/len(c)
         
