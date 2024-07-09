08f497d8de951b7160f0ec4fefd66747cc3d48d5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-09 23:55:20 +0900

    Time: 790 ms (14.78%), Space: 57.1 MB (45.36%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -4,12 +4,8 @@ class Solution:
         w=0
         ans=0.0
         for a,b in customers:
-            if a>w:
-                ans+=b
-                w=a+b
-            else:
-                w+=b
-                ans=ans+(w-a)
+            w =  max(w, a) + b
+            ans += (w - a)
 
         return ans/len(customers)
         
