2c8eb2a24b4579b554f7f998d7fd15cfd9e40478
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-10 14:06:45 +0900

    Time: 48 ms (63.33%), Space: 16.7 MB (36.19%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.py
+++ 1720-crawler-log-folder/1720-crawler-log-folder.py
@@ -2,11 +2,10 @@ class Solution:
     def minOperations(self, logs: List[str]) -> int:
         ans=0
         for p in logs:
-            
-            if p.startswith('..') and p.endswith('/'):
+            if p=='../':
                 if ans>0:
                     ans-=1
-            elif p.endswith('/') and p!='./':
+            elif p!='./':
                 ans+=1
         
         return ans
