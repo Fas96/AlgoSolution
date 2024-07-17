d58b538ceae8b38966d2348d5ddb5cdd2f06fa66
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-17 23:36:57 +0900

    Time: 68 ms (9.44%), Space: 17.1 MB (24.3%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -5,12 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        s = to_delete
+    def delNodes(self, root: Optional[TreeNode], dl: List[int]) -> List[TreeNode]:
+      
         res = []
         def dfs(root, flag):
             if not root: return None
-            toDelete = (root.val in s)
+            toDelete = (root.val in dl)
             root.left = dfs(root.left, toDelete)
             root.right = dfs(root.right, toDelete)
             if not toDelete and flag: res.append(root)
