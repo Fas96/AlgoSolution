99d5e94bf5d96ac5bd491e536503ef9101345f51
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-19 11:12:54 +0900

    Time: 110 ms (60.82%), Space: 16.8 MB (79.04%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -1,6 +1,11 @@
+'''
+*matrix decomposes it into separate lists each representing a row for example if matrix =[[1,2], [3, 4]] then *matrix returns two lists [1,2] and [3, 4]
+zip([1, 2], [3, 4]) returns two lists of [1, 3] and [2, 4] which are the columns of the matrix
+'''
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        minrow = {min(r) for r in matrix}
-      
-        maxcol = {max(c) for c in zip(*matrix)}
-        return list(minrow & maxcol)
+        rows = {min(r) for r in matrix}
+
+        cols = {max(c) for c in zip(*matrix)}
+   
+        return list(rows & cols)
