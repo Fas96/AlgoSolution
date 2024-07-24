2d0c8034e46540dcde4cc42745db17f0240a85aa
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-24 11:53:40 +0900

    Time: 1172 ms (35.87%), Space: 25 MB (41.3%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -1,17 +1,7 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        def digitsA(n):
-            ar = []
-            while n > 0:
-                digit = n % 10
-                ar.append(str(mapping[digit]))
-                n = n // 10
-            
-            return int(''.join(ar)[::-1],base=10)
         ss=[]
         for n in nums:
-            if n==0:
-                ss.append(mapping[n])
-            else: ss.append(digitsA(n))
+            ss.append(int(''.join(str(mapping[int(d)]) for d in str(n))))
            
         return [k for k,v in sorted(zip(nums,ss),key=lambda a:a[1])]
