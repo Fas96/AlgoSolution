1b832bf9e2b98d58bf2aa7430716f6777d12854f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-26 19:42:17 +0900

    Time: 521 ms (91.44%), Space: 28.9 MB (38.95%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,23 +1,19 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        mp=defaultdict(list)
-
-        idx=0
-        for t,f in edges:
-            mp[t].append((f,succProb[idx]))
-            mp[f].append((t,succProb[idx]))
-            idx+=1
-        def djk(node):
-             
+        mp=defaultdict(list) 
+        for [t,f],p in zip(edges,succProb):
+            mp[t].append((f,p))
+            mp[f].append((t,p)) 
+        def djk(node): 
             prq=[(-1,node)]
             visited = set()
             while prq:
-                curDistance,curNode=heapq.heappop(prq) 
-                if curNode==end_node:  return -curDistance
+                cp,curNode=heapq.heappop(prq) 
+                if curNode==end_node:  return -cp
                 if curNode in visited: continue
                 visited.add(curNode) 
                 for neighbor,prob in mp[curNode]:
-                    heapq.heappush(prq,(curDistance*prob,neighbor))
+                    heapq.heappush(prq,(cp*prob,neighbor))
             return 0
          
         return djk(start_node)
