8d1b07dde74532e8647ec0c0a5e265a4828dafa5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-26 20:37:51 +0900

    Time: 1398 ms (63.24%), Space: 97.1 MB (9.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,14 +1,14 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
         def soe(n): 
-            is_prime = [True] * (n + 1) 
-            if n >= 0: is_prime[0] = False
-            if n >= 1: is_prime[1] = False 
+            fl = [True] * (n + 1) 
+            if n >= 0: fl[0] = False
+            if n >= 1: fl[1] = False 
             for i in range(2, int(n**0.5) + 1):
-                if is_prime[i]:
+                if fl[i]:
                     for j in range(i * i, n + 1, i):
-                        is_prime[j] = False
-            return is_prime 
+                        fl[j] = False
+            return fl 
         flg = soe(n)
          
         return len([b for b in flg[:len(flg)-1] if b]) 
