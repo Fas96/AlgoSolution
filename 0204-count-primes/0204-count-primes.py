5292e9bbaea6579c1dbe223e27eefec0b63ddef5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-26 20:34:53 +0900

    Time: 1817 ms (45.8%), Space: 108.1 MB (6.56%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,28 +1,16 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n<=2: return 0
-       
-        def prime_sieve(n): 
-            flag = n % 6 == 2
-            sieve = bytearray((n // 3 + flag >> 3) + 1)
-            for i in range(1, int(n**0.5) // 3 + 1):
-                if not (sieve[i >> 3] >> (i & 7)) & 1:
-                    k = (3 * i + 1) | 1
-                    for j in range(k * k // 3, n // 3 + flag, 2 * k):
-                        sieve[j >> 3] |= 1 << (j & 7)
-                    for j in range(k * (k - 2 * (i & 1) + 4) // 3, n // 3 + flag, 2 * k):
-                        sieve[j >> 3] |= 1 << (j & 7)
-            return sieve
-        def prime_list(n): 
-            res = []
-            if n > 1:
-                res.append(2)
-            if n > 2:
-                res.append(3)
-            if n > 4:
-                sieve = prime_sieve(n + 1)
-                res.extend(3 * i + 1 | 1 for i in range(1, (n + 1) // 3 + (n % 6 == 1)) if not (sieve[i >> 3] >> (i & 7)) & 1)
-            return res
-        ll=prime_list(n)
-        return len(ll)-1 if ll[-1]==n else len(ll)
-        
+        def sieve_of_eratosthenes(n): 
+            is_prime = [True] * (n + 1) 
+            if n >= 0:
+                is_prime[0] = False
+            if n >= 1:
+                is_prime[1] = False 
+            for i in range(2, int(n**0.5) + 1):
+                if is_prime[i]:
+                    for j in range(i * i, n + 1, i):
+                        is_prime[j] = False
+            return is_prime 
+        prime_flags = sieve_of_eratosthenes(n)
+         
+        return len([i for i, prime in enumerate(prime_flags[:len(prime_flags)-1]) if prime]) 
