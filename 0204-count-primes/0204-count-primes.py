f870c2a93a682e10fd8c1c3e3959e5a5edc93017
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-07-26 20:35:37 +0900

    Time: 1854 ms (44.41%), Space: 108.2 MB (6.56%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,10 +2,8 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         def sieve_of_eratosthenes(n): 
             is_prime = [True] * (n + 1) 
-            if n >= 0:
-                is_prime[0] = False
-            if n >= 1:
-                is_prime[1] = False 
+            if n >= 0: is_prime[0] = False
+            if n >= 1: is_prime[1] = False 
             for i in range(2, int(n**0.5) + 1):
                 if is_prime[i]:
                     for j in range(i * i, n + 1, i):
