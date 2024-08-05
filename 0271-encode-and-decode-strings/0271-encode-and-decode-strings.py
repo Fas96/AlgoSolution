80a28fdeba094d47fd19f2943038575e942e079f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-08-05 19:32:17 +0900

    Time: 65 ms (71.15%), Space: 16.8 MB (88.87%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,19 +2,23 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
+        print(''.join('%d:' % len(s) + s for s in strs))
         return ''.join('%d:' % len(s) + s for s in strs)
+        
+
+        
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
-        """
-        strs = []
-        i = 0
-        while i < len(s):
-            j = s.find(':', i)
-            i = j + 1 + int(s[i:j])
-            strs.append(s[j+1:i])
-        return strs
+        """  
+        i,n=0,len(s)
+        ans=[]
+        while i<n:
+            j=s.find(':',i)
+            i=j + 1 + int(s[i:j])
+            ans.append(s[j+1:i])
+        return  ans
         
 
 
