c8befdb19ab6e358be087b995c4e777a5399a99b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-08-11 19:33:15 +0900

    Time: 22 ms (99.39%), Space: 16.5 MB (68.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,3 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        m_fact = factorial(m-1)
-        n_fact = factorial(n-1)
-        return factorial(n+m-2)//(m_fact*n_fact)
+    def uniquePaths(self, m: int, n: int) -> int: 
+        return factorial(n+m-2)//(factorial(m-1)*factorial(n-1))
