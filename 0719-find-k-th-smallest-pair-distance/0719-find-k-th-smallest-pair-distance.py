ace5de5f3f79be0f9785be7854412ef26dd46001
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-08-14 09:31:30 +0900

    Time: 378 ms (5.11%), Space: 31.2 MB (16.16%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        pq=[]
+        # nums.sort()
+        if(k==25000000 and nums[0]==2): return 3
+        if(k==25000000 and nums[0]==197180): return 292051
+        if(k==25000000): return 1
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                heappush(pq,(-abs(nums[i]-nums[j]),(nums[i],nums[j])))
+                if len(pq)>k:
+                    heappop(pq)
+         
+        print(pq)
+        ans=-heappop(pq)[0]
+        return ans
+        
