6153969fe5c564b04e521ff3aceaadab8008a7e3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-01 12:53:09 +0900

    Time: 663 ms (88.51%), Space: 23.2 MB (99.44%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def construct2DArray(self, og: List[int], m: int, n: int) -> List[List[int]]:
         if m*n != len(og): return []
-        ans=[] 
-        ans=[]
+        ans=[]  
         for i in range(0,m*n,n):
             ans.append(og[i:i+n])
         return ans
