2d445207578da808ca2c8bbf460922f2faccbe2f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-21 20:27:45 +0900

    Time: 86 ms (34.09%), Space: 23.3 MB (53.23%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,4 +1,3 @@
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        a_list=[str(i) for i in range(1,n+1)] 
-        return [int(i) for i in sorted(a_list, key = lambda s : s) ]
+    def lexicalOrder(self, n: int) -> List[int]: 
+        return [int(i) for i in sorted([str(i) for i in range(1,n+1)] , key = lambda s : s) ]
