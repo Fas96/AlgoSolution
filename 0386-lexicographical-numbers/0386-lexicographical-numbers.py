362f099ab8ffa94b658ded1c4b4668ec7c4f224a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-21 20:27:31 +0900

    Time: 78 ms (49.76%), Space: 24 MB (34.33%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,4 +1,4 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        return sorted(range(1, n+1), key=lambda x: str(x))
-        
+        a_list=[str(i) for i in range(1,n+1)] 
+        return [int(i) for i in sorted(a_list, key = lambda s : s) ]
