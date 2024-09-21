d8518198da2e43f275b156e316a64b68c9be2a88
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-21 20:31:46 +0900

    Time: 42 ms (99.88%), Space: 22.5 MB (66.63%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,3 +1,14 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]: 
-        return [int(i) for i in sorted([str(i) for i in range(1,n+1)] , key = lambda s : s) ]
+        al = []
+        curr = 1
+        for i in range(1, n + 1):
+            al.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr % 10 == 9 or curr >= n:
+                    curr //= 10
+                curr += 1
+        return al
+        # return [int(i) for i in sorted([str(i) for i in range(1,n+1)] , key = lambda s : s) ]
