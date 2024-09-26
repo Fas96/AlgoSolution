a15ee67c41f645437905d43047a3703e341bda5b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-26 23:02:47 +0900

    Time: 324 ms (31.67%), Space: 17.3 MB (89.44%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,17 +1,13 @@
 class MyCalendar:
 
     def __init__(self):
-        self.events = []
+        self.bookings = []  
 
-    def book(self, start: int, end: int) -> bool:
-        for s, e in self.events:
-            if s < end and start < e:
+    def book(self, start: int, end: int) -> bool: 
+        for existingStart, existingEnd in self.bookings: 
+            if start < existingEnd and end > existingStart:
                 return False
-        self.events.append((start, end))
+         
+        self.bookings.append((start, end))
         return True
-        
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+ 
