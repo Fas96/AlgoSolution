0c04c777c8eee4f79cd4fd8f419ef40ce23e01f6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-09-27 23:46:13 +0900

    Time: 3633 ms (5.06%), Space: 17.5 MB (45.71%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,24 +1,15 @@
 class MyCalendarTwo:
-
     def __init__(self):
-        self.events = []
-
+        self.mp = defaultdict(int)
     def book(self, start: int, end: int) -> bool:
-        # 3 overlaps
-        overlaps = []
-        for s, e in self.events:
-            if s < end and start < e:
-                overlaps.append((max(s, start), min(e, end)))
-        # 2 overlaps
-        for i in range(len(overlaps)):
-            for j in range(i + 1, len(overlaps)):
-                if overlaps[i][0] < overlaps[j][1] and overlaps[j][0] < overlaps[i][1]:
-                    return False
-        self.events.append((start, end))
+        self.mp[start] += 1
+        self.mp[end] -= 1
+
+        ActiveCount = 0
+        for value, count in sorted(self.mp.items()):
+            ActiveCount += count
+            if ActiveCount > 2:
+                self.mp[start] -= 1
+                self.mp[end] += 1
+                return False
         return True
-        
-
-
-# Your MyCalendarTwo object will be instantiated and called as such:
-# obj = MyCalendarTwo()
-# param_1 = obj.book(start,end)
