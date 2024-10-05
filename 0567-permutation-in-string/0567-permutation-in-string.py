522d0d1b4404aea5a410f9e37cb970a583aac299
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-10-06 00:42:45 +0900

    Time: 76 ms (43.92%), Space: 16.9 MB (22.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,21 +1,14 @@
-from itertools import permutations
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n = len(s1)
-        h2 = [0]*26
-        h1 = [0]*26
-        
-        for c in s1: 
-            h1[ord(c) - ord('a')] += 1
-        
-        for i in range(len(s2)):
-            if i < n:
-                h2[ord(s2[i]) - ord('a')] += 1
-            else:
-                h2[ord(s2[i]) - ord('a')] += 1
-                h2[ord(s2[i-n]) - ord('a')] -= 1
-                
-            if h1 == h2: return True
-            
+        n1, n2=len(s1), len(s2)
+        if n2<n1: return False
+        freq1, freq2 = Counter(s1), Counter(s2[0:n1])
+        if freq1==freq2: return True
+        l,  r = 1, n1
+        while r<n2:
+            freq2[s2[l-1]]-=1
+            freq2[s2[r]]+=1
+            if freq1==freq2: return True
+            r+=1
+            l+=1
         return False
-        
