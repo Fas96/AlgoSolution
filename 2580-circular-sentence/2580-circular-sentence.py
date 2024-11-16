df88f4ccf39702ae066ad28f344fd714481afda5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-11-16 22:35:00 +0900

    Time: 4 ms (14.63%), Space: 16.7 MB (19.31%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.py
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -3,8 +3,7 @@ class Solution:
         ans=True
         n=len(s.split(" "))
         s=s.split(" ")
-        for i in range(n-1):
-            print(s[i][-1],s[i+1][0])
+        for i in range(n-1): 
             if i+1<n and s[i][-1]!=s[i+1][0]:
                 ans=False
         return s[0][0]==s[len(s)-1][-1] and ( ans)
