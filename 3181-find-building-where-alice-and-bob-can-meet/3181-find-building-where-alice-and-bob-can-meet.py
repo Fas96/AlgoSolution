6805eebf8f5c937cd3f25e12dd27e05fab5cbfc3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-12-22 23:20:50 +0900

    Time: 222 ms (100%), Space: 40.8 MB (63.92%) - LeetHub

--- 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.py
+++ 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.py
@@ -1,39 +1,24 @@
 class Solution:
-    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        n = len(heights)
-        st = [[0] * 20 for _ in range(n)]
-        Log = [-1] * (n + 1)
-        Log[0] = -1
-        for i in range(1, n + 1):
-            Log[i] = Log[i >> 1] + 1
+    def leftmostBuildingQueries(self, heights, queries):
+        n, q = len(heights), len(queries)
+        result = [-1] * q
+        deferred = [[] for _ in range(n)]
+        pq = []
+
+        for i in range(q):
+            a, b = queries[i]
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                result[i] = b
+            else:
+                deferred[b].append((heights[a], i))
+
         for i in range(n):
-            st[i][0] = heights[i]
-        for i in range(1, 20):
-            for j in range(n):
-                if j + (1 << i) <= n:
-                    st[j][i] = max(st[j][i - 1], st[j + (1 << (i - 1))][i - 1])
+            for query in deferred[i]:
+                heapq.heappush(pq, query)
+            while pq and pq[0][0] < heights[i]:
+                result[pq[0][1]] = i
+                heapq.heappop(pq)
 
-        def Ask(l, r):
-            k = Log[r - l + 1]
-            return max(st[l][k], st[r - (1 << k) + 1][k])
-
-        res = []
-        for l, r in queries:
-            if l > r:
-                l, r = r, l
-            if l == r:
-                res.append(l)
-                continue
-            if heights[r] > heights[l]:
-                res.append(r)
-                continue
-            max_height = max(heights[r], heights[l])
-            left, right = r + 1, n
-            while left < right:
-                mid = (left + right) // 2
-                if Ask(r + 1, mid) > max_height:
-                    right = mid
-                else:
-                    left = mid + 1
-            res.append(left if left != n else -1)
-        return res
+        return result
