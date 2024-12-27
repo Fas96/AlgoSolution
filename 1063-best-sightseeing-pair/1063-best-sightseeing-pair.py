20111d4105c50086024b1efa8636cb5d315adeb3
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2024-12-27 15:15:53 +0900

    Time: 71 ms (49.76%), Space: 23.4 MB (6.34%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         n=len(values)
-        maxIScore,score=0,0
+        maxIScore,ans=0,0
         for i,x in enumerate(values):
-            score=max(score,maxIScore+x-i)
+            ans=max(ans,maxIScore+x-i)
             maxIScore=max(maxIScore,x+i)
-        return score
+        return ans
