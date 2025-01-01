5bcaa7d7ee97b9919876cc282fa287a7158fe8fd
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-01-01 12:24:49 +0900

    Time: 3 ms (18.71%), Space: 8.9 MB (23.35%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxScore(std::string s) {
+        int max_score = 0;
+        for (int i = 1; i < s.length(); ++i) {
+            int left_count = std::count(s.begin(), s.begin() + i, '0');
+            int right_count = std::count(s.begin() + i, s.end(), '1');
+            max_score = std::max(max_score, left_count + right_count);
+        }
+        return max_score;
+    }
+};
