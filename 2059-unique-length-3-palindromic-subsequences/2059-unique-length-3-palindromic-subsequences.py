1876e7346d3c2585d1013b2fb0fbc36f019689a9
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-01-04 11:06:58 +0900

    Time: 197 ms (76.26%), Space: 22.2 MB (8.35%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,15 @@
+
+from itertools import combinations
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+
+        fq = defaultdict(list)
+        for i, char in enumerate(s):
+            fq[char]+=[i]
+
+        count = 0
+        for indices in fq.values():
+            if len(indices) > 1:
+                unique_chars = set(s[min(indices)+1:max(indices)])
+                count += len(unique_chars)
+        return count
