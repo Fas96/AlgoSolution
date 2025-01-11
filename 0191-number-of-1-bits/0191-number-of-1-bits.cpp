e90781e9cbd37867627f260d608136fa98bd2dd9
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-01-11 19:50:14 +0900

    Time: 0 ms (100%), Space: 8.4 MB (9.29%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-     
-        int ans; 
-            while(n != 0){
-                if(n&1)ans+=1;
-                n= n>>1;
-            } 
-        
-        return ans;
- 
+        return __builtin_popcount(n);
     }
 };
