0afa6d52e5c73960a3c35636720a7d0e44bcde6c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-01-11 19:51:39 +0900

    Time: 2 ms (33.77%), Space: 11.4 MB (15.83%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,13 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(auto i=0;i<=n;i++){
-            int cnt=0;
-            int t=i;
-            while(t != 0){
-                if(t&1)cnt+=1;
-                t= t>>1;
-            }
-            ans.push_back(cnt);
+            ans.push_back(__builtin_popcount(i));
         }
         return ans;
     }
