64a44d8a6a1f9bfbf5267b1980fcefa8fefeebe6
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-01-20 23:28:38 +0900

    Time: 61 ms (99.37%), Space: 38.9 MB (99.05%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
@@ -1,25 +1,19 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        num_rows, num_cols = len(mat), len(mat[0])
-        row_count, col_count = [0] * num_rows, [0] * num_cols
-        num_to_pos = {}
+        m, n=len(mat), len(mat[0])
+        N=m*n
 
-        # Create a map to store the position of each number in the matrix
-        for row in range(num_rows):
-            for col in range(num_cols):
-                num_to_pos[mat[row][col]] = [row, col]
-
-        for i in range(len(arr)):
-            num = arr[i]
-            row, col = num_to_pos[num]
-
-            # Increment the count for the corresponding row and column
-            row_count[row] += 1
-            col_count[col] += 1
-
-            # Check if the row or column is completely painted
-            if row_count[row] == num_cols or col_count[col] == num_rows:
+        to_i=[-1]*(N+1)
+        to_j=[-1]*(N+1)
+        for i, row in enumerate(mat):
+            for j, x in enumerate(row):
+                to_i[x]=i
+                to_j[x]=j
+        R=[0]*m
+        C=[0]*n
+        for i, x in enumerate(arr):
+            R[to_i[x]]+=1
+            C[to_j[x]]+=1
+            if R[to_i[x]]==n or C[to_j[x]]==m:
                 return i
-
-        # This line will never be reached as per the problem statement
         return -1
