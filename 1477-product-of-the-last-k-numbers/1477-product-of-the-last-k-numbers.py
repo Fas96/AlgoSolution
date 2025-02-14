df1d78696493c7d5900c2f6aaeba9ad4f2f2cdc2
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-02-14 19:58:02 +0900

    Time: 1029 ms (9.29%), Space: 31.5 MB (88.13%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -1,17 +1,12 @@
-from itertools import accumulate
 from operator import mul
 
 class ProductOfNumbers:
     def __init__(self):
         self.nms = [1]  
+
     def add(self, num: int) -> None:
-        if num == 0:
-            self.nms = [1]   
-        else:
-            self.nms.append(self.nms[-1] * num)
+        self.nms = [1] if num == 0 else self.nms + [self.nms[-1] * num]
 
     def getProduct(self, k: int) -> int:
-        if k >= len(self.nms):  
-            return 0
-        return self.nms[-1] // self.nms[-(k+1)]
+        return 0 if k >= len(self.nms) else self.nms[-1] // self.nms[-(k+1)]
  
