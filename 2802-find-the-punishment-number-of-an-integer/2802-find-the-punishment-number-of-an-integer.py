a898407dc07f21b23ac4606874219f113323d63f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-02-15 18:58:49 +0900

    Time: 1019 ms (59.23%), Space: 17.7 MB (68.75%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        ans=0
+        def isSumofIntegersSubstringEqualsI(s, target, start=0, current_sum=0):
+            if start == len(s):
+                return current_sum == target
+            
+            for end in range(start + 1, len(s) + 1):
+                num = int(s[start:end])
+                if isSumofIntegersSubstringEqualsI(s, target, end, current_sum + num):
+                    return True
+            return False
+        for i in range(1,n+1):
+            if isSumofIntegersSubstringEqualsI(str(i*i), i):
+                ans+=(i*i)
+        return ans
+
+        
