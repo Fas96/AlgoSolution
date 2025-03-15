1400fdad5dcc649c97285063890dc3a6f292f27e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-03-15 22:29:24 +0900

    Time: 300 ms (68.7%), Space: 27.9 MB (99.79%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        x0=min(nums)
+        xM=max(nums)
+        def f(cap):
+            steal, i=0, 0
+            while i<n and steal<=k:
+                if nums[i]<=cap:
+                    steal+=1
+                    i+=1
+                i+=1
+            return steal>=k
+
+        l, r=x0, xM
+        while l<r:
+            m=(l+r)>>1
+            if f(m):
+                r=m
+            else:
+                l=m+1
+        return l
+        
