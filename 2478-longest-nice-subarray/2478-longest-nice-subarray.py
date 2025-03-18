70e5d013a45b744b49e67f832d25c1bdb7c0f389
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-03-18 16:34:00 +0900

    Time: 94 ms (50.3%), Space: 32 MB (57.93%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -3,15 +3,14 @@ from typing import List
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         ans = 1  
-        i, e, n = 0, 0, len(nums)
+        end,    n = 0,  len(nums)
         used_bits = 0   
 
-        for e in range(n):
-            while (used_bits & nums[e]) != 0:  
-                used_bits ^= nums[i]  
-                i += 1  
-            
-            used_bits |= nums[e]  
-            ans = max(ans, e - i + 1) 
+        for s in range(n):
+            while (used_bits & nums[s]) != 0:  
+                used_bits ^= nums[end]  
+                end += 1   
+            used_bits |= nums[s]  
+            ans = max(ans,s - end + 1) 
         
         return ans
