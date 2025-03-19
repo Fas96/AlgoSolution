2e58d6b19c09f7236d3bc945a7babe428dc97130
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-03-19 23:31:48 +0900

    Time: 96 ms (71.98%), Space: 21.5 MB (96.4%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -3,12 +3,10 @@ class Solution:
         ans,i,ln=0,0,len(nums)
         for i in range(ln-2):
             if nums[i]==0:
-                ans+=1 
-                for j in range(i,i+3):
-                    if nums[j]==0:
-                        nums[j]=1
-                    else:
-                        nums[j]=0 
+                ans+=1
+                nums[i] ^= 1
+                nums[i + 1] ^= 1
+                nums[i + 2] ^= 1
         
         return -1 if 0 in nums[ln-2:] else ans
         
