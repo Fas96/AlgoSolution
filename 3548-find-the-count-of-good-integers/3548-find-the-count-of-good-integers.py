60af9097dc5c6a5849e1da5b6e768dee3520a369
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-04-13 01:16:48 +0900

    Time: 895 ms (77.42%), Space: 18.9 MB (96.77%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
@@ -1,13 +1,39 @@
 class Solution:
     def countGoodIntegers(self, n: int, k: int) -> int:
-        return ([
-            [9, 9, 243, 252, 10935, 10944, 617463, 617472, 41457015, 41457024],
-            [4, 4, 108, 172, 7400, 9064, 509248, 563392, 37728000, 39718144],
-            [3, 3, 69, 84, 3573, 3744, 206217, 207840, 13726509, 13831104],
-            [2, 2, 54, 98, 4208, 6992, 393948, 494818, 33175696, 37326452],
-            [1, 1, 27, 52, 2231, 3256, 182335, 237112, 15814071, 19284856],
-            [1, 1, 30, 58, 2468, 3109, 170176, 188945, 12476696, 13249798],
-            [1, 1, 33, 76, 2665, 3044, 377610, 506388, 36789447, 40242031],
-            [1, 1, 27, 52, 2231, 5221, 292692, 460048, 30771543, 35755906],
-            [1, 1, 23, 28, 1191, 1248, 68739, 69280, 4623119, 4610368]
-        ])[k - 1][n - 1]
+        data = set()
+        half = (n+1) // 2
+        start = 10 ** (half - 1)
+        end = 10 ** half
+
+        for h in range(start, end):
+            h = str(h)
+            if n%2 == 1:
+                s = h + h[:-1][::-1] 
+            else:
+                s = h + h[::-1] 
+            
+            if int(s) % k == 0:
+                data.add("".join(sorted(s)))
+        
+        fact = [1] * (n+1)
+        for i in range(1, n+1):
+            fact[i] = fact[i-1] * i
+
+        total = 0
+        for s in data:
+            count = [0]*10
+            for ch in s:
+                count[int(ch)] += 1
+
+            perms = 0
+            for d in range(1, 10):
+                if count[d] == 0:
+                    continue
+                count[d]-=1
+                p = fact[n-1]
+                for c in count:
+                    p = p // fact[c]
+                perms = perms + p
+                count[d]+=1
+            total = total + perms
+        return total
