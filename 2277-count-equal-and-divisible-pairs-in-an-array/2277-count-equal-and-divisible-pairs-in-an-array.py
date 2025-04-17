f08da9ca093dd1c07337af0b859fd0b5e15a16cf
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-04-17 23:29:54 +0900

    Time: 24 ms (25.33%), Space: 17.7 MB (95.67%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,11 +1,4 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-
-        n=len(nums)
-        ans=0
-        for i in range(n):
-            for j in range(i+1,n): 
-                if nums[i]==nums[j] and  ((i*j)%k)==0:
-                    ans+=1
-        return ans
+        return sum(1 for i in range(len(nums)) for j in range(i+1, len(nums)) if nums[i] == nums[j] and (i * j) % k == 0)
         
