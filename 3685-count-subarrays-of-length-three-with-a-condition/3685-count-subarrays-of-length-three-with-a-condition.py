8f952f3920e2cb566f69f09cfa4424b27078386c
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-04-28 00:00:18 +0900

    Time: 8 ms (40.82%), Space: 17.8 MB (45.92%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
@@ -4,12 +4,9 @@ from typing import List
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
         ans = 0
-        n = len(nums)
-        seen = set()
-        
-        for i in range(n - 2):
-            subarray = tuple(nums[i:i+3])  # using tuple as it's hashable
-            a, b, c = subarray
+        n = len(nums) 
+        for i in range(n - 2): 
+            a, b, c = tuple(nums[i:i+3]) 
             if a + c == b / 2: 
                 ans += 1
         return ans
