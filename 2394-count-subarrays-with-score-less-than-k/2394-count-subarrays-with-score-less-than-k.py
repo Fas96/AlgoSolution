4fe012870082fbd520303f1012be5b6c8c213e16
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-04-28 23:44:58 +0900

    Time: 120 ms (54.65%), Space: 30.7 MB (31.97%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -3,13 +3,13 @@ class Solution:
         left = 0
         current_sum, count = 0, 0
 
-        for right in range(len(nums)):
-            current_sum += nums[right]
+        for i,n in enumerate( nums):
+            current_sum += n
 
-            while  current_sum * (right - left + 1) >= k:
+            while  current_sum * (i - left + 1) >= k:
                 current_sum -= nums[left]
                 left += 1
             
-            count += right - left + 1
+            count += i - left + 1
 
         return count
