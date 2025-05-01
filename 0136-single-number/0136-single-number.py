6d7ca60d6d4fa81040a193d849809f33a701416a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-05-01 17:25:05 +0900

    Time: 0 ms (100%), Space: 19.5 MB (77.48%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ans=0
-        for n in nums:
-            ans= n ^ ans
+        for x in nums:
+            ans^=x
         return ans
         
