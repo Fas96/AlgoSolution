f538d6b8fac0b7b75ab731a1d1613c190d798979
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-05-04 21:16:54 +0900

    Time: 12 ms (53.07%), Space: 24.4 MB (43.37%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -4,9 +4,10 @@ class Solution:
         for domino in dominoes:
             normalized = tuple(sorted(domino))
             domino_counts[normalized] += 1
+        print(domino_counts)
         
-        total_pairs = 0
+        ans = 0
         for count in domino_counts.values():
             if count >= 2:
-                total_pairs += count * (count - 1) // 2
-        return total_pairs
+                ans += count * (count - 1) // 2
+        return ans
