a22728eb85720d5f8e4f33e5727584a5cb038af7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-05-08 00:11:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -3,22 +3,29 @@ import heapq
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         n = len(moveTime)
-        m = len(moveTime[0])
-
-        h = [(0, 0, 0)]
-        dist = {(0, 0): 0}
-
-        while h:
-            t, x, y = heapq.heappop(h)
-            if dist.get((x, y), float('inf')) < t:
-                continue
+        m = len(moveTime[0]) if n > 0 else 0
+        if n == 0 or m == 0:
+            return 0
+        
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+        heap = []
+        heapq.heappush(heap, (moveTime[0][0], 0, 0))
+        
+        dist = [[float('inf')] * m for _ in range(n)]
+        dist[0][0] = moveTime[0][0]
+        
+        while heap:
+            current_time, x, y = heapq.heappop(heap)
             if x == n - 1 and y == m - 1:
-                return t
-            
-            for u, v in ((x - 1, y), (x, y - 1),
-                         (x + 1, y), (x, y + 1)):
-                if 0 <= u < n and 0 <= v < m:
-                    next_t = max(t, moveTime[u][v]) + 1
-                    if dist.get((u, v), float('inf')) > next_t:
-                        dist[(u, v)] = next_t
-                        heapq.heappush(h, (next_t, u, v))
+                return current_time
+            if current_time > dist[x][y]:
+                continue
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m:
+                    new_time = max(current_time , moveTime[nx][ny])+ 1
+                    if dist[nx][ny] >new_time :
+                        dist[nx][ny] = new_time
+                        heapq.heappush(heap, (new_time, nx, ny))
+        return dist[n-1][m-1] if dist[n-1][m-1] != float('inf') else -1
