85fb7040292203974edfda5e38a9d43d1a625ede
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-05-23 23:45:14 +0900

    Time: 60 ms (34.48%), Space: 25.8 MB (100%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.py
@@ -1,5 +1,5 @@
 class Solution:
-     def maximumValueSum(self, A: List[int], k: int, edges: List[List[int]]) -> int:
+    def maximumValueSum(self, A: List[int], k: int, edges: List[List[int]]) -> int:
         res = c = 0
         d = 1 << 30
         for a in A:
