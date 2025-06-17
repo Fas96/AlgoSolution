6c012e2639ce4373b885f48633dc02cd42d21103
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-06-17 18:56:55 +0900

    Time: 151 ms (62.53%), Space: 46.6 MB (6.24%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t < 0: return False # edge case 
+        
+        seen = {}
+        for i, x in enumerate(nums): 
+            bkt = x//(t+1)
+            if bkt in seen and i - seen[bkt][0] <= k: return True 
+            if bkt-1 in seen and i - seen[bkt-1][0] <= k and abs(x - seen[bkt-1][1]) <= t: return True 
+            if bkt+1 in seen and i - seen[bkt+1][0] <= k and abs(x - seen[bkt+1][1]) <= t: return True 
+            seen[bkt] = (i, x) 
+        return False 
