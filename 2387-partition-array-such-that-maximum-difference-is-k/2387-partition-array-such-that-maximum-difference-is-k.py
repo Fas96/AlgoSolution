11393c3956e0c76a03142381f251ed3dc52e799f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-06-19 15:32:04 +0900

    Time: 93 ms (41.24%), Space: 29 MB (73.61%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
@@ -1,8 +1,8 @@
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         nums.sort()
-        startIndex,n=0,len(nums)
-        numOfSubSeq=0
+        n=len(nums)
+        numOfSubSeq=startIndex=0
         while startIndex<n:
             endOfGroup=nums[startIndex]+k
             startIndex=bisect_right(nums,endOfGroup,startIndex)
