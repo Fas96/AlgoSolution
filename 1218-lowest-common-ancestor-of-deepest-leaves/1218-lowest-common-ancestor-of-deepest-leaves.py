132974b24b109a493bf5e0c716a76fa1347a501e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-06-20 02:19:53 +0900

    Time: 7 ms (13.16%), Space: 18.3 MB (45.15%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -6,9 +6,19 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def f(n):
-            if not n: return 0,None
-            ld, ln, rd, rn = *f(n.left), *f(n.right)
-            return ((ld+1,n),(ld+1,ln),(rd+1,rn))[(ld>rd)-(ld<rd)]
-
-        return f(root)[1]
+        def treeHeight(node):
+            if not node:
+                return 0
+            return 1+max(treeHeight(node.left),treeHeight(node.right))
+        def dfs(node,maxH,L):
+            if not node:
+                return None
+            if maxH-1==L:
+                return node
+            left=dfs(node.left,maxH,L+1)
+            right=dfs(node.right,maxH,L+1)
+            if left and right:
+                return node
+            return left if left else right
+        maxi=treeHeight(root)
+        return dfs(root,maxi,0)
