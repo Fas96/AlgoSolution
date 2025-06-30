d70275e879fa9f91b9efc5da18dff7bac12bde74
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-06-30 18:27:17 +0900

    Time: 8068 ms (5.03%), Space: 18.9 MB (85.83%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -3,11 +3,12 @@ class Solution:
         nums.sort()
         l,r=0,len(nums)-1
         ans=0 
-        while l< len(nums):
+        n=len(nums)
+        while l< n:
             if nums[l]+1 not in nums:
                 l+=1
                 continue
-            end=bisect_right(nums,nums[l]+1)-l 
+            end=bisect_right(nums,nums[l]+1,lo=l,hi=n)-l 
             ans=max(ans,end) 
             l+=1
         return ans 
