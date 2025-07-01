4e2d4934ecafb40a3422d188b7a6f4ed6eaab960
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-07-01 13:54:32 +0900

    Time: 28 ms (97.18%), Space: 17.6 MB (72.11%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -1,7 +1,10 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
         freq=Counter(word)
-        if len(freq)==1:return len(word)
-        mistakes=[word[i]!=word[i-1] for i in range(1, len(word))]
-        return len(word)-sum(mistakes)
+        N=len(word)
+        if len(freq)==1:return N
+        for i in range(1, len(word)):
+            if word[i]!=word[i-1]:
+                N-=1
+        return N
         
