2b21533f29da1bcdf23f890ad077da68b575a86b
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-07-03 16:56:33 +0900

    Time: 0 ms (100%), Space: 17.7 MB (85.53%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,7 +1,5 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        if k == 1:
-            return 'a'
-        pos = k - 1 
-        increment = bin(pos).count('1')
+        if k == 1: return 'a' 
+        increment = bin(k - 1).count('1')
         return chr(ord('a') + (increment % 26))
