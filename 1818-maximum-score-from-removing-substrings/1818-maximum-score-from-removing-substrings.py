38518097220bed789822fbee9c512abb49e039af
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-07-23 22:19:08 +0900

    Time: 9990 ms (5.16%), Space: 18.1 MB (94.19%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
@@ -2,22 +2,23 @@ class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
         points = 0
         
-        def remove_substrings(s, sub, point):
-            nonlocal points
-            stack = []
-            for char in s:
-                stack.append(char)
-                if len(stack) >= 2 and stack[-2] + stack[-1] == sub:
-                    stack.pop()
-                    stack.pop()
-                    points += point
-            return ''.join(stack)
+        def remove_and_count(s, sub, point):
+            count = 0
+            while sub in s:
+                num_sub = s.count(sub)
+                count += num_sub
+                s = s.replace(sub, '', num_sub)
+            return s, count * point
         
         if y > x:
-            s = remove_substrings(s, 'ba', y)
-            s = remove_substrings(s, 'ab', x)
+            s, gained = remove_and_count(s, 'ba', y)
+            points += gained
+            s, gained = remove_and_count(s, 'ab', x)
+            points += gained
         else:
-            s = remove_substrings(s, 'ab', x)
-            s = remove_substrings(s, 'ba', y)
+            s, gained = remove_and_count(s, 'ab', x)
+            points += gained
+            s, gained = remove_and_count(s, 'ba', y)
+            points += gained
         
         return points
