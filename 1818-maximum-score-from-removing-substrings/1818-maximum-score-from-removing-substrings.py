39f218aca52ce7e7caaeedb82d2295130bccd75e
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-07-23 22:16:49 +0900

    Time: 368 ms (16.13%), Space: 19.2 MB (76.13%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
@@ -1,28 +1,23 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
         points = 0
-        a, b = 'a', 'b'
+        
+        def remove_substrings(s, sub, point):
+            nonlocal points
+            stack = []
+            for char in s:
+                stack.append(char)
+                if len(stack) >= 2 and stack[-2] + stack[-1] == sub:
+                    stack.pop()
+                    stack.pop()
+                    points += point
+            return ''.join(stack)
         
         if y > x:
-            a, b = b, a
-            x, y = y, x
-        
-        stack = []
-        # First pass to remove 'ab' substrings
-        for char in s:
-            if char == b and stack and stack[-1] == a:
-                stack.pop()
-                points += x
-            else:
-                stack.append(char)
-        
-        # Second pass to remove 'ba' substrings from the remaining string
-        remaining = []
-        for char in stack:
-            if char == a and remaining and remaining[-1] == b:
-                remaining.pop()
-                points += y
-            else:
-                remaining.append(char)
+            s = remove_substrings(s, 'ba', y)
+            s = remove_substrings(s, 'ab', x)
+        else:
+            s = remove_substrings(s, 'ab', x)
+            s = remove_substrings(s, 'ba', y)
         
         return points
