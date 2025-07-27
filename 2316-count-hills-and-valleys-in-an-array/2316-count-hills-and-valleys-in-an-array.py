5fdf30891abb5e207a198f6c946d8f693bc3fbff
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-07-27 12:57:02 +0900

    Time: 0 ms (100%), Space: 17.8 MB (64.58%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -8,20 +8,21 @@ class Solution:
         while i < n - 1:
             left = i - 1
             right = i + 1 
+            #move left i until wedont have same value as current num
             while left >= 0 and nums[left] == nums[i]:
                 left -= 1
             if left < 0:
                 i += 1
                 continue 
+            # Understand we move until no nums are same with right
             while right < n and nums[right] == nums[i]:
                 right += 1
             if right >= n:
                 i += 1
                 continue
-            if (nums[left] < nums[i] and nums[right] < nums[i]) or \
-               (nums[left] > nums[i] and nums[right] > nums[i]):
+            #We check from left or right if we have hill or Valley
+            if (nums[left] < nums[i] and nums[right] < nums[i]) or (nums[left] > nums[i] and nums[right] > nums[i]):
                 hv += 1
                  
             i = right
-            
         return hv
