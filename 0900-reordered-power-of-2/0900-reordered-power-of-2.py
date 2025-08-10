e6b5fc1da8db80370e9a5e096bd6b3f2dff5a004
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-08-10 21:44:14 +0900

    Time: 4 ms (42.37%), Space: 18 MB (17.56%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.py
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -1,18 +1,14 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        p2=[]
-        for i in range(31):
-            p2.append(2**i)
+        pow=[str(2**i) for i in range(31)] 
 
-        N,fn=len(str(n)),Counter(str(n)) 
-        fp=defaultdict(int)
-        p=[]
-        for pp in p2:
-            if len(str(pp))==N:
-                p.append(pp)
-        for c in p:
-            cc=Counter(str(c)) 
-            if cc==fn:
+        N,freqN=len(str(n)),Counter(str(n)) 
+         
+        eqN=[pp for pp in pow if len(pp)==N]
+       
+        for c in eqN:
+            cc=Counter(c) 
+            if cc==freqN:
                 return True
         return False 
         
