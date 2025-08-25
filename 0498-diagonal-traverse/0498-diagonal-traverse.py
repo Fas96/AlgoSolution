f2bdcbfde5a963d14b2ecf02534ba7555005e9b5
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-08-25 11:00:23 +0900

    Time: 3437 ms (5.03%), Space: 19.7 MB (59.95%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        if not mat or not mat[0]:
+            return []
+        
+        r, c = len(mat), len(mat[0])
+        result = [] 
+        for diagonal in range(r + c - 1):
+            temp = [] 
+            for i in range(r):
+                j = diagonal - i
+                if 0 <= j < c: 
+                    temp.append(mat[i][j])
+            
+            if diagonal % 2 == 0:
+                temp.reverse()
+            
+            result.extend(temp)
+        
+        return result
+        # [0,0]
+
+        # [0,1],[1,0]
+
+        # [0,2],[1,1],[0,2]
+
+        # [1,2],[2,2],
+
+        # [2,3]
+
+        return []
