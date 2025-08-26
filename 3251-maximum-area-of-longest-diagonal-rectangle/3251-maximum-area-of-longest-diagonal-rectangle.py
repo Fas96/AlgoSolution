9234aba08868d902a96622cbefb6b78c7e1d182a
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-08-26 11:40:44 +0900

    Time: 0 ms (100%), Space: 18.1 MB (22.6%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
@@ -1,5 +1,3 @@
 class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
-        ans=sorted([[sqrt(w*w+l*l),l*w] for w,l in dimensions],key=lambda x: (x[0], x[1]))
-         
-        return  ans[-1][1]
+        return  sorted([[sqrt(w*w+l*l),l*w] for w,l in dimensions],key=lambda x: (x[0], x[1]))[-1][1]
