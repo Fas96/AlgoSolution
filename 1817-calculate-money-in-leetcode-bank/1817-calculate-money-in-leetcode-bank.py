fbac11e01c9ddd80681cafb73c88f8dde87dcf80
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-10-25 22:36:27 +0900

    Time: 0 ms (100%), Space: 17.7 MB (89.38%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
@@ -1,14 +1,6 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        ans=0
-        monday=0
-        while n>0:
-            if n>7:
-                ans += (7 * (1 + monday) + 21)
-                n-=7
-            else: 
-                for i in range(1,n+1): 
-                    ans+=(i+monday)
-                break
-            monday+=1
+        weeks, days = divmod(n, 7)
+        ans = 28 * weeks + 7 * weeks * (weeks - 1) // 2
+        ans += days * (weeks + 1) + days * (days - 1) // 2
         return ans
