68ed9ece766ee5c912e7ca0f26227ffbd6980dec
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-11-21 19:56:35 +0900

    Time: 167 ms (64.96%), Space: 22.7 MB (10.49%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -6,9 +6,10 @@ class Solution(object):
                 char_indices[char] = []
             char_indices[char].append(i)
 
+ 
         count = 0
         for indices in char_indices.values():
             if len(indices) > 1:
-                unique_chars = set(s[min(indices)+1:max(indices)])
+                unique_chars = set(s[min(indices)+1:max(indices)]) 
                 count += len(unique_chars)
         return count
