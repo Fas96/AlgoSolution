1ba20cea445f31a33f16c3be4e946b6dc3c0f999
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-11-22 18:55:39 +0900

    Time: 3 ms (12.99%), Space: 17.9 MB (21.95%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.py
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -1,8 +1,6 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         ans = 0
-        for x in nums:
-            if x % 3 == 0:
-                continue
+        for x in nums: 
             ans += min(x % 3, 3 - (x % 3))
         return ans
