f4d63b1a879198c8cd2965a12b02f5a2b74bde5f
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-12-23 14:20:22 +0900

    Time: 169 ms (82.28%), Space: 56.3 MB (96.46%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        n=len(events)
-        Time=[(0, 0, 0)]*(n*2)
-        for i, (s,e,v) in enumerate(events):
-            Time[2*i]=(s, False, v)
-            Time[2*i+1]=(e, True, v)
-        Time.sort()
-        ans, maxV=0, 0
-        for t, isEnd, v in Time:
-            if isEnd: maxV=max(maxV, v)
-            else: ans=max(ans, maxV+v)
+        events.sort(key=lambda x: x[1])
+        first_event_end = [0]
+        first_event_val = [0] # Best up to corresponding end time
+        max_seen = 0
+        for start, end, val in events:
+            max_seen = max(max_seen, val)
+            first_event_end.append(end)
+            first_event_val.append(max_seen)
+
+        events.sort(key=lambda x: x[0])
+        cur = 0
+        ans = 0
+
+        for start, end, val in events:
+            while first_event_end[cur + 1] < start:
+                cur += 1
+            ans = max(ans, val + first_event_val[cur])
+
         return ans
