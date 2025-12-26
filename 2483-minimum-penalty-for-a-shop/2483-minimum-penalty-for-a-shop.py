b778efe5ce0b657396854c74acf1b89780c73a64
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2025-12-26 16:19:24 +0900

    Time: 43 ms (94.41%), Space: 17.8 MB (98.14%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,18 +1,14 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-         
-        if 'N' not in customers:return len(customers)
-        if 'Y' not in customers:return 0
+        bestTime = 0
+        minPenalty = 0
+        prefix = 0
         
-        penalty=0
-        mx=0
-        minPenal=-1
         for i in range(len(customers)):
-            penalty += 1 if customers[i] == 'Y' else -1
-            if penalty > mx:
-                mx=penalty
-                minPenal=i
-        return minPenal+1
-                
+            prefix += -1 if customers[i] == 'Y' else 1
             
+            if prefix < minPenalty:
+                bestTime = i + 1
+                minPenalty = prefix
         
+        return bestTime
