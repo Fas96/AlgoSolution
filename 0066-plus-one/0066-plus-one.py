fbb55e7124cbdcad7ea9e9966148035d8d54b222
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2026-01-01 15:44:01 +0900

    Time: 0 ms (100%), Space: 17.6 MB (63.3%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        def toSre(s):
+        def toStr(s):
             return str(s)
-        s="".join(map(toSre,digits))
+        s="".join(map(toStr,digits))
         sm=int(s)+1
-        def digitss(v):
+        def getDigits(v):
             an=[]
             while v>0:
                 r=v%10
                 v//=10
                 an.append(r)
             return an
-        return digitss(sm)[::-1]
+        return getDigits(sm)[::-1]
         
