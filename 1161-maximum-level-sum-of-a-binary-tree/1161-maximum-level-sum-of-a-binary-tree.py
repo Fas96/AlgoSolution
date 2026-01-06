d6f7e6c7bd9e02ec1b1d2ae64e1bb3a08f4ed2fe
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2026-01-06 23:58:07 +0900

    Time: 32 ms (26.78%), Space: 22.8 MB (13.07%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -6,13 +6,16 @@
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        if not root: return 0
+        if not root:
+            return 0
         
-        q=deque([(root,1)])
-        ls={}
-        while q:
-            n,l=q.popleft()
-            ls[l]=ls.get(l,0)+n.val
-            if n.left: q.append((n.left,l+1))
-            if n.right: q.append((n.right,l+1))
-        return max(ls,key=ls.get)
+        ls = {}
+        
+        def dfs(node, level):
+            if not node:
+                return
+            ls[level] = ls.get(level, 0) + node.val
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+        dfs(root, 1)
+        return max(ls, key=ls.get)
