53376b9805d4db09e59be13e32725de103af8204
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2026-01-06 23:54:06 +0900

    Time: 39 ms (17.38%), Space: 22.9 MB (13.07%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -5,36 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxLevelSum(self, root: Optional[TreeNode]) -> int: 
-        def bfs(root):
-            if not root:
-                return []
-
-            r = []
-            q = deque([root])   
-
-            while q:
-                ls = len(q)
-                cln = [] 
-                for _ in range(ls):
-                    node = q.popleft()
-                    cln.append(node.val) 
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right) 
-                r.append(cln)
-
-            return r
-         
-        ans=float("-inf")
-        rans=1
-        prev=-1
-        for i, v in enumerate(bfs(root)):
-            ans=max(ans,sum(v))
-            if ans>prev:
-                rans=i+1 
-            prev=ans
-        return rans
-
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
         
+        q=deque([(root,1)])
+        ls={}
+        while q:
+            n,l=q.popleft()
+            ls[l]=ls.get(l,0)+n.val
+            if n.left: q.append((n.left,l+1))
+            if n.right: q.append((n.right,l+1))
+        return max(ls,key=ls.get)
