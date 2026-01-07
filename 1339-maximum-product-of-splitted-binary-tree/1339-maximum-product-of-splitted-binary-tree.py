9270d33d9c2286d3f06eb1d56ae7576cd486fe27
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2026-01-07 16:12:32 +0900

    Time: 35 ms (96.2%), Space: 46.3 MB (5.32%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -7,19 +7,24 @@
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
         MOD = 10**9 + 7
-        def totalTree(node):
+        subtree_sums = []
+        
+        def dfs(node):
             if not node:
                 return 0
-            return node.val+totalTree(node.left)+totalTree(node.right)
-        totalSumofTree=totalTree(root)
-        self.maxAns=0
-        def dfs(node):
-            if not node: return 0
-            ls=dfs(node.left)
-            rs=dfs(node.right)
-            ss = node.val + ls + rs
-            otherSizeSum=totalSumofTree-ss
-            self.maxAns=max(otherSizeSum*ss,self.maxAns)
-            return ss
-        dfs(root)
-        return self.maxAns %MOD
+            
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+            subtree_sum = node.val + left_sum + right_sum
+            
+            subtree_sums.append(subtree_sum)
+            return subtree_sum
+        
+        total_sum = dfs(root)
+        
+        max_product = 0
+        for subtree_sum in subtree_sums:
+            product = subtree_sum * (total_sum - subtree_sum)
+            max_product = max(max_product, product)
+        
+        return max_product % MOD
