cc4f28da62545f8111fb47ed95adb33f80f704e7
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2026-01-11 07:56:36 +0900

    Time: 259 ms (84.72%), Space: 23.8 MB (27.84%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,13 +1,25 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        mp=collections.defaultdict(int)
         m, n = len(s1), len(s2)
-        for i in range(m):
-            for j in range(n):
-                if s1[i]==s2[j]:
-                    mp[i,j]=mp[i-1,j-1]+ord(s1[i])
-                else:
-                    mp[i,j]=max(mp[i-1,j],mp[i,j-1])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
         
-        t_ascii=sum(map(ord,s1))+sum(map(ord,s2))
-        return t_ascii-2*mp[m-1,n-1]
+        for i in range(1, m + 1):
+            dp[i][0] = dp[i-1][0] + ord(s1[i-1])
+        
+    
+        for j in range(1, n + 1):
+            dp[0][j] = dp[0][j-1] + ord(s2[j-1])
+        
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else: 
+                    dp[i][j] = min(
+                        dp[i-1][j] + ord(s1[i-1]),  # delete from s1
+                        dp[i][j-1] + ord(s2[j-1])   # delete from s2
+                    )
+        
+        return dp[m][n]
+        
